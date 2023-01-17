b1d0df0ac68d10841c00f6027c2bc2283645bcdb
Jeffrey Alan Scudder <me@jas.life>
2023-01-17 03:27:06 -0500

    more patreon tests

--- system/netlify/functions/sotce-diary.js
+++ system/netlify/functions/sotce-diary.js
@@ -11,16 +11,97 @@ const dev = process.env.NETLIFY_DEV;
 // const patreonRefreshToken = process.env.SOTCE_PATREON_REFRESH_TOKEN;
 
 import crypto from "crypto"; // Import crypto library.
+import { patreon as patreonAPI, oauth as patreonOAuth } from "patreon";
 
 async function fun(event, context) {
+  const client = process.env.SOTCE_PATREON_CLIENT_ID;
+  const secret = process.env.SOTCE_PATREON_CLIENT_SECRET;
+
   const host = dev ? "localhost" : "sotce.aesthetic.computer";
   const origin = dev
     ? "https://localhost:8888/sotce-diary"
     : "https://sotce.aesthetic.computer";
   const redirect = "https://sotce.aesthetic.computer"; // This also needs to be set on the Patreon developer account.
 
+  const code = event.queryStringParameters.code;
+
   // 1. Return an entry page if it's a GET.
   if (event.httpMethod === "GET") {
+    // If the code parameter is present, exchange it for an access token
+    if (code) {
+      //const { got } = await import("got"); // Import the "got" http library.
+
+      // const patreonAPI = patreon(client, secret);
+      // console.log(patreonAPI)
+
+      const patreonOAuthClient = patreonOAuth(client, secret);
+
+
+      try {
+        const tokensResponse = await patreonOAuthClient.getTokens(
+          code,
+          redirect
+        );
+        console.log("Tokens...", tokensResponse);
+      } catch (err) {
+        // TODO: Show an access desnied page?
+        console.error("Error!");
+        return {
+          statusCode: 500,
+          body: "Error!"
+        }
+      }
+
+      const accessToken = tokensResponse.access_token;
+
+      // const tokens = await patreonAPI.exchangeCodeForToken(code, redirect);
+      // const accessToken = tokens.access_token;
+
+      // TODO: How to I make a post request using the patreon library with the code, to
+      // see what tier this user is in in order to redirect them below according to
+      // whether they have access or not.
+
+      // Redirect the user to a new URL with the access token
+      return {
+        statusCode: 307,
+        headers: {
+          Location: `https://sotce.aesthetic.computer/?access_token=${accessToken}`,
+        },
+      };
+    }
+
+    // If the code parameter is not present, return the HTML template with the authorization link
+    const state = crypto.randomBytes(32).toString("hex");
+    const loginUrl = `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${client}&redirect_uri=${redirect}&state=${state}`;
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+      body: `
+      <h1>Sotce's Diary</h1>
+      <a href="${loginUrl}">Enter</a>
+    `,
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: "Unauthorized HTTP Method",
+    };
+  }
+}
+
+export const handler = fun;
+// Authorization URL
+
+// TODO: How to change loginURL to the
+
+/*
+    let loginUrl = `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${client}&redirect_uri=${redirect}&state=${crypto
+      .randomBytes(32)
+      .toString("hex")}`;
+
     return {
       statusCode: 200,
       headers: {
@@ -28,19 +109,7 @@ async function fun(event, context) {
       },
       body: `
         <h1>Sotce's Diary</h1>
-        <button id="submit-button">Submit</button>
-        <script>
-          document.getElementById("submit-button").addEventListener("click", async () => {
-            // Add your fetch request here
-            const response = await fetch('${origin}', {
-              method: 'POST',
-              body: JSON.stringify({ tier: 'your_tier' }),
-              headers: { 'Content-Type': 'application/json' },
-            });
-            const data = await response.json();
-            console.log(data);
-          });
-        </script>
+        <a href="${loginUrl}">Enter</button>
       `,
     };
   } else if (event.httpMethod !== "POST") {
@@ -50,47 +119,31 @@ async function fun(event, context) {
       body: "Unauthorized HTTP Method",
     };
   }
+  */
 
-  const client = process.env.SOTCE_PATREON_CLIENT_ID;
-  const secret = process.env.SOTCE_PATREON_CLIENT_SECRET;
+// TODO: How can I change the returned html based on the presence of the code attribute to trigger a post request
+//       or redirect the user for authorization?
 
-  // Assume POST
-  return {
-    statusCode: 307,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      Location: `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${client}&redirect_uri=${redirect}&scope=users&state=${crypto
-        .randomBytes(32)
-        .toString("hex")}`,
-    },
-  };
+//const isSubscribed = true;
 
-  // 3. Otherwise identify the user.
-  // const { got } = await import("got"); // Import the "got" http library.
+// Return the result
+//return {
+//  statusCode: 200,
+//  body: JSON.stringify({ isSubscribed }),
+//};
 
-  // Send the request to the Patreon OAuth2 authorization endpoint
+// 3. Otherwise identify the user.
+// const { got } = await import("got"); // Import the "got" http library.
 
-  // const { body } = await got({
-  //   method: "GET",
-  //   url: "https://www.patreon.com/oauth2/authorize",
-  //   searchParams: {
-  //     response_type: "code",
-  //     client_id: client,
-  //     redirect_uri: redirect,
-  //     scope: "users",
-  //     state: crypto.randomBytes(32).toString("hex"),
-  //   },
-  //   followRedirect: false,
-  // });
-  // Extract the code from the redirect URL
+// Send the request to the Patreon OAuth2 authorization endpoint
 
-  // const url = new URL(body);
-  // const code = url.searchParams.get("code");
+// const url = new URL(body);
+// const code = url.searchParams.get("code");
 
-  // console.log(url, code);
+// console.log(url, code);
 
-  // Get the access token
-  /*
+// Get the access token
+/*
   const { body } = await got.post("https://www.patreon.com/api/oauth2/token", {
     json: true,
     form: {
@@ -105,7 +158,7 @@ async function fun(event, context) {
   console.log(body);
   */
 
-  /*
+/*
   // Extract the access token from the response
   const access_token = body.access_token;
   // Extract the patron_id from the response
@@ -130,14 +183,3 @@ async function fun(event, context) {
     (membership) => membership.attributes.tier_name === tier
   );
   */
-
-  const isSubscribed = true;
-
-  // Return the result
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ isSubscribed }),
-  };
-}
-
-export const handler = fun;
