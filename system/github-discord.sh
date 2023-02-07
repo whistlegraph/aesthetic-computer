b8e30ee5fe2ee56cda43e46cd8e30ce365b2036d
Jeffrey Alan Scudder <me@jas.life>
2023-02-07 18:17:10 -0500

    unquote bitflag in discord hook

--- system/github-discord.sh
+++ system/github-discord.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-WEBHOOK_URL=$DISCORD_WEBHOOK_URL
-# Define the Discord webhook URL
+WEBHOOK_URL=$DISCORD_WEBHOOK_URL # Define the Discord webhook URL
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 COMMIT_URL="$REPOSITORY_URL/commit/$COMMIT_REF"
 SHORT_HASH="${COMMIT_REF:0:7}"
 DISCORD_MESSAGE="[$SHORT_HASH]($COMMIT_URL) · $COMMIT_MESSAGE️ · [aesthetic.computer]($URL)"
-FLAGS="\"2\"" # Define the message flags (SUPPRESS_EMBEDS) - https://discord.com/developers/docs/resources/channel#message-object-message-flags
+FLAGS="2" # Define the message flags (SUPPRESS_EMBEDS) - https://discord.com/developers/docs/resources/channel#message-object-message-flags
 
 # Send the POST request to the Discord webhook URL
 curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"$DISCORD_MESSAGE\",\"flags\":$FLAGS}" $WEBHOOK_URL
