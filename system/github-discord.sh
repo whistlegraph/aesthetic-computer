8ac853b96e61bad5bfacb48d6f88607197b49d86
Jeffrey Alan Scudder <me@jas.life>
2023-02-07 18:32:49 -0500

    link in code

--- system/github-discord.sh
+++ system/github-discord.sh
@@ -4,7 +4,7 @@ WEBHOOK_URL=$DISCORD_WEBHOOK_URL # Define the Discord webhook URL
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 COMMIT_URL="$REPOSITORY_URL/commit/$COMMIT_REF"
 SHORT_HASH="${COMMIT_REF:0:7}"
-DISCORD_MESSAGE="[$SHORT_HASH](<$COMMIT_URL>) [aesthetic.computer](<$URL>) · $COMMIT_MESSAGE️ 🖋️"
+DISCORD_MESSAGE="`[$SHORT_HASH](<$COMMIT_URL>)` [aesthetic.computer](<$URL>) · $COMMIT_MESSAGE️ 🖋️"
 FLAGS="\"2\"" # Define the message flags (SUPPRESS_EMBEDS) - https://discord.com/developers/docs/resources/channel#message-object-message-flags
 
 # Send the POST request to the Discord webhook URL
