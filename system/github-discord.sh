bade519e260e801862ecb1956e88fb86245d634d
Jeffrey Alan Scudder <me@jas.life>
2023-02-07 17:22:10 -0500

    more webhook updates

--- system/github-discord.sh
+++ system/github-discord.sh
@@ -6,5 +6,8 @@ WEBHOOK_URL=$DISCORD_WEBHOOK_URL
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 DISCORD_MESSAGE="system updated to \`$COMMIT_REF\` - $COMMIT_MESSAGE - $(netlify open)"
 
+echo $DISCORD_MESSAGE
+echo $DISCORD_WEBHOOK_URL
+
 # Send the POST request to the Discord webhook URL
 curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"$DISCORD_MESSAGE\", \"disable_web_page_preview\": true}" $WEBHOOK_URL
