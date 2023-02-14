12fe3e5eaf5ccd001250005ab13cbf330ec81ab1
Jeffrey Alan Scudder <me@jas.life>
2023-02-13 21:18:05 -0500

    specify `valbear`

---
+++ system/public/aesthetic.computer/disks/valbear.mjs
@@ -0,0 +1,54 @@
+// Valbear, 23.02.13.21.15 
+// Make your valentine a card with a cute bear! 
+
+/* #region 🚴 TODO 
+  - [💝] Add every button / wire up all the actions!
+  - [] Add meta function!
+  - [] Test on some different screen sizes / make it rly compact!
+
+  How to make/send a custom Valentine’s bear buddy for/to your pal/loved one on a.c
+    1. grass = rect
+    2. sky = rect 
+    3. smear
+    4. body = oval
+    5. heart = shape , trail = heart wand
+    6. words (happy V day!/ will you b mine?/ i < 3 u/ i love you, xoxo) = word / xoxo)
+    7. download
+    8. reset
+
+#endregion */
+
+// 🥾 Boot (Runs once before first paint and sim)
+function boot($) {
+}
+
+// 🎨 Paint (Executes every display frame)
+function paint($) {
+}
+
+/*
+// ✒ Act (Runs once per user interaction)
+function act({ event }) {
+  // Respond to user input here.
+}
+
+// 🧮 Sim(ulate) (Runs once per logic frame (120fps locked)).
+function sim($api) {
+  // Crunch numbers outside of rendering here.
+}
+
+// 💗 Beat (Runs once per bpm, starting when the audio engine is activated.)
+function beat($api) {
+  // Make sound here.
+}
+
+// 👋 Leave (Runs once before the piece is unloaded)
+function leave($api) {
+  // Pass data to the next piece here.
+}
+*/
+
+// 📚 Library (Useful functions used throughout the piece)
+// ...
+
+export { boot, paint }
