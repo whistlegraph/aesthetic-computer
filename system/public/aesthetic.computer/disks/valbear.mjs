928d6a45e4f912335873ff19b101b27a0d2279e4
Jeffrey Alan Scudder <me@jas.life>
2023-02-14 15:50:04 -0500

    abstract Button into TextButton in `valbear` and `ui`

--- system/public/aesthetic.computer/disks/valbear.mjs
+++ system/public/aesthetic.computer/disks/valbear.mjs
@@ -3,9 +3,9 @@
 
 /* #region 🚴 TODO 
   + Cleanup
-  - [] Abstract basic button code so it's not redundant.
   - [] Abstract 'no' and 'no!' code so it's not redundant.
   + Done
+  - [x] Abstract basic button code so it's not redundant.
   - [x] Add every button / wire up all the actions!
   - [x] Add meta function!
   - [x] Test on some different screen sizes / make it rly compact!
@@ -29,7 +29,7 @@ let grass,
 function boot() {}
 
 // 🎨 Paint (Executes every display frame)
-function paint({ wipe, system, ink, num, ui: { Button }, screen }) {
+function paint({ wipe, system, ink, ui: { TextButton: TB }, screen }) {
   const y = 25;
   const x = 6;
 
@@ -46,133 +46,73 @@ function paint({ wipe, system, ink, num, ui: { Button }, screen }) {
   let xs = 6;
   const gap = 4;
   const g2 = gap * 2;
-  const offset = { x: gap, y: gap };
-  const h = 19;
-  const cw = 6; // Character width.
 
   // Grass
-  const gTxt = "Grass";
-  const gw = gTxt.length * cw + g2;
-  grass = new Button({ x: x + xs, y: y + ys, w: gw, h });
-  ink(0, 100, 0)
-    .box(grass.box, "fill")
-    .ink(0, 255, 0, 150)
-    .box(grass.box, "outline")
-    .ink(0, 200, 0)
-    .write(gTxt, num.p2.add(grass.box, offset), [0, 50, 0]);
+  grass = new TB("Grass", { x: x + xs, y: y + ys });
+  grass.paint({ ink });
 
   // Sky
-  const sTxt = "Sky";
-  sky = new Button({
-    x: x + xs + gw + g2,
-    y: y + ys,
-    w: sTxt.length * cw + g2,
-    h,
-  });
-  ink(0, 0, 100)
-    .box(sky.box, "fill")
-    .ink(0, 0, 255, 150)
-    .box(sky.box, "outline")
-    .ink(100, 100, 255)
-    .write(sTxt, num.p2.add(sky.box, offset), [0, 0, 50]);
+  sky = new TB("Sky", { x: x + xs + grass.btn.box.w + g2, y: y + ys });
+  sky.paint({ ink }, [
+    [0, 0, 100],
+    [0, 0, 255, 150],
+    [100, 100, 255],
+    [0, 0, 50],
+  ]);
 
   // Smear
-  const smTxt = "Smear";
-  smear = new Button({
-    x: sky.box.x + sky.box.w + g2,
-    y: y + ys,
-    w: smTxt.length * cw + g2,
-    h,
-  });
-  ink(64)
-    .box(smear.box, "fill")
-    .ink(127, 150)
-    .box(smear.box, "outline")
-    .ink(200)
-    .write(smTxt, num.p2.add(smear.box, offset), [50]);
+  smear = new TB("Smear", { x: sky.btn.box.x + sky.btn.box.w + g2, y: y + ys });
+  smear.paint({ ink }, [[64], [127, 150], [200], [50]]);
 
   ys += 28;
 
   // Bear Body
-  const bTxt = "Bear Body";
-  body = new Button({
-    x: x + xs,
-    y: y + ys,
-    w: bTxt.length * cw + g2,
-    h,
-  });
-  ink(100, 50, 0)
-    .box(body.box, "fill")
-    .ink(250, 150, 10)
-    .box(body.box, "outline")
-    .ink(100, 50, 0)
-    .write(bTxt, num.p2.add(body.box, offset), [255, 100, 0]);
+  body = new TB("Bear Body", { x: x + xs, y: y + ys });
+  body.paint({ ink }, [
+    [100, 50, 0],
+    [250, 150, 10],
+    [100, 50, 0],
+    [255, 100, 0],
+  ]);
 
   // Lines
-  const lTxt = "Detail Lines";
-  lines = new Button({
-    x: body.box.x + body.box.w + g2,
+  lines = new TB("Detail Lines", {
+    x: body.btn.box.x + body.btn.box.w + g2,
     y: y + ys,
-    w: lTxt.length * cw + g2,
-    h,
   });
-  ink(127)
-    .box(lines.box, "fill")
-    .ink(200)
-    .box(lines.box, "outline")
-    .ink(100)
-    .write(lTxt, num.p2.add(lines.box, offset), [200]);
+  lines.paint({ ink }, [[127], [200], [100], [200]]);
 
   ys += 28;
 
   // Heart
-  const hTxt = "Freehand Heart";
-  heart = new Button({
-    x: x + xs,
-    y: y + ys,
-    w: hTxt.length * cw + g2,
-    h,
-  });
-  ink(100, 0, 0)
-    .box(heart.box, "fill")
-    .ink(250, 0, 0)
-    .box(heart.box, "outline")
-    .ink(100, 0, 0)
-    .write(hTxt, num.p2.add(heart.box, offset), [255, 0, 0]);
+  heart = new TB("Freehand Heart", { x: x + xs, y: y + ys });
+  heart.paint({ ink }, [
+    [100, 0, 0],
+    [250, 0, 0],
+    [100, 0, 0],
+    [255, 0, 0],
+  ]);
 
   // Trail
-  const htTxt = "<3 Trail";
-  trail = new Button({
-    x: heart.box.x + heart.box.w + g2,
+  trail = new TB("<3 Trail", {
+    x: heart.btn.box.x + heart.btn.box.w + g2,
     y: y + ys,
-    w: htTxt.length * cw + g2,
-    h,
   });
-  ink(100, 80, 80)
-    .box(trail.box, "fill")
-    .ink(250, 200, 200)
-    .box(trail.box, "outline")
-    .ink(100, 90, 90)
-    .write(htTxt, num.p2.add(trail.box, offset), [255, 200, 200]);
+  trail.paint({ ink }, [
+    [100, 80, 80],
+    [250, 200, 200],
+    [100, 90, 90],
+    [255, 200, 200],
+  ]);
 
   ys += 28;
 
   // Words
-  const wTxt = "Sweet Nothings";
-  words = new Button({
-    x: x + xs,
-    y: y + ys,
-    w: wTxt.length * cw + g2,
-    h,
-  });
-  ink(50)
-    .box(words.box, "fill")
-    .ink(100)
-    .box(words.box, "outline")
-    .ink(50)
-    .write(wTxt, num.p2.add(words.box, offset), [127]);
+  words = new TB("Sweet Nothings", { x: x + xs, y: y + ys });
+  words.paint({ ink }, [[50], [100], [50], [127]]);
 
   ys += 54;
+
   ink(255, 255, 0, 100).write("(try each, in order)", {
     x: xs + 4,
     y: ys - 2,
@@ -182,68 +122,42 @@ function paint({ wipe, system, ink, num, ui: { Button }, screen }) {
   ink(255, 255, 0, 100).line(0, ys + by, screen.width, ys + by);
 
   // Download
-  const dTxt = "Download Valentine";
-  download = new Button({
-    x: x + xs,
-    y: y + ys,
-    w: dTxt.length * cw + g2,
-    h,
-  });
-  ink(40)
-    .box(download.box, "fill")
-    .ink(180)
-    .box(download.box, "outline")
-    .ink(200)
-    .write(dTxt, num.p2.add(download.box, offset), [20]);
+  download = new TB("Download Valentine", { x: x + xs, y: y + ys });
+  download.paint({ ink }, [[40], [180], [200], [20]]);
 
   ys += 28;
 
   // Undo
-  const uTxt = "Undo";
-  undo = new Button({
-    x: x + xs,
-    y: y + ys,
-    w: uTxt.length * cw + g2,
-    h,
-  });
-  ink(20, 0, 0)
-    .box(undo.box, "fill")
-    .ink(120, 60, 0)
-    .box(undo.box, "outline")
-    .ink(200, 100, 0)
-    .write(uTxt, num.p2.add(undo.box, offset), [10, 0, 0]);
+  undo = new TB("Undo", { x: x + xs, y: y + ys });
+  undo.paint({ ink }, [
+    [20, 0, 0],
+    [120, 60, 0],
+    [200, 100, 0],
+    [10, 0, 0],
+  ]);
 
   // Reset
-  const rTxt = "Reset";
-  reset = new Button({
-    x: undo.box.x + undo.box.w + g2,
+  reset = new TB("Reset", {
+    x: undo.btn.box.x + undo.btn.box.w + g2,
     y: y + ys,
-    w: rTxt.length * cw + g2,
-    h,
   });
-  ink(40, 0, 0)
-    .box(reset.box, "fill")
-    .ink(180, 0, 0)
-    .box(reset.box, "outline")
-    .ink(200, 0, 0)
-    .write(rTxt, num.p2.add(reset.box, offset), [20, 0, 0]);
+  reset.paint({ ink }, [
+    [40, 0, 0],
+    [180, 0, 0],
+    [200, 0, 0],
+    [20, 0, 0],
+  ]);
 
   ys += 28;
 
   // Help
-  const heTxt = "Help";
-  discord = new Button({
-    x: x + xs,
-    y: y + ys,
-    w: heTxt.length * cw + g2,
-    h,
-  });
-  ink(0, 0, 40)
-    .box(discord.box, "fill")
-    .ink(0, 0, 180)
-    .box(discord.box, "outline")
-    .ink(80, 80, 255)
-    .write(heTxt, num.p2.add(discord.box, offset), [0, 0, 20]);
+  discord = new TB("Help", { x: x + xs, y: y + ys });
+  discord.paint({ ink }, [
+    [0, 0, 40],
+    [0, 0, 180],
+    [80, 80, 255],
+    [0, 0, 20],
+  ]);
 
   return false;
 }
@@ -262,21 +176,21 @@ function act(
   },
   num
 ) {
-  grass.act(e, () => jump(`rect 0 ${rr(100, 200)} 0`));
-  sky.act(e, () => jump(`rect 150 ${rr(100, 150)} ${rr(200, 255)}`));
-  smear.act(e, () => jump(`smear ${rr(4, 32)}`));
-  body.act(e, () => {
+  grass.btn.act(e, () => jump(`rect 0 ${rr(100, 200)} 0`));
+  sky.btn.act(e, () => jump(`rect 150 ${rr(100, 150)} ${rr(200, 255)}`));
+  smear.btn.act(e, () => jump(`smear ${rr(4, 32)}`));
+  body.btn.act(e, () => {
     help.flip() ? jump("oval 150-200 60-100 10-40 230-255") : jump("oval");
   });
-  lines.act(e, () => jump(`line:${rr(1, 4)} ? ? ?`));
+  lines.btn.act(e, () => jump(`line:${rr(1, 4)} ? ? ?`));
 
-  heart.act(e, () =>
+  heart.btn.act(e, () =>
     jump(`shape ${rr(100, 255)} 0 ${rr(20, 255)}, ${(rr, 200, 255)}`)
   );
 
-  trail.act(e, () => jump(`sparkle`));
+  trail.btn.act(e, () => jump(`sparkle`));
 
-  words.act(e, () => {
+  words.btn.act(e, () => {
     const scale = rr(1, 3);
     jump(
       `word:${scale}:${scale} ${help.choose(
@@ -296,7 +210,7 @@ function act(
     );
   });
 
-  download.act(e, () => {
+  download.btn.act(e, () => {
     if (store["painting"]) {
       dl(`valentine-${timestamp()}.png`, store["painting"], {
         scale: 6,
@@ -305,7 +219,7 @@ function act(
     }
   });
 
-  undo.act(e, () => {
+  undo.btn.act(e, () => {
     // Ripped straight from prompt!
     const paintings = system.nopaint.undo.paintings;
 
@@ -338,7 +252,7 @@ function act(
     }
   });
 
-  reset.act(e, async () => {
+  reset.btn.act(e, async () => {
     await store.delete("painting", "local:db");
     system.nopaint.undo.paintings.length = 0; // Reset undo stack.
 
@@ -347,7 +261,7 @@ function act(
     needsPaint();
   });
 
-  discord.act(e, () => jump("https://discord.gg/aesthetic-computer"));
+  discord.btn.act(e, () => jump("https://discord.gg/aesthetic-computer"));
 }
 
 function meta() {
