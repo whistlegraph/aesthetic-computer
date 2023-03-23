6457f3b2c036ff43f46c6e5a8abaeac568c7a1c2
Jeffrey Alan Scudder <me@jas.life>
2023-03-23 04:13:10 -0400

    quote url

--- video-sync/autostart-child.sh
+++ video-sync/autostart-child.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # See also: `autostart.sh`
-chromium-browser --kiosk /home/georgica/aesthetic.computer/video-sync/index.html?ip=192.168.1.1 
+chromium-browser --kiosk '/home/georgica/aesthetic.computer/video-sync/index.html?ip=192.168.1.1' 
