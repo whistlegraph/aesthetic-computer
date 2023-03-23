7520c95df2880d5dedfeed0e79ab27390336e24e
Jeffrey Alan Scudder <me@jas.life>
2023-03-23 04:05:53 -0400

    docs

--- video-sync/autostart.sh
+++ video-sync/autostart.sh
@@ -2,7 +2,7 @@
 
 # Installation to run this file on boot:
 # chmod +x autostart.sh
-# cp autostart.sh /etc/init.d/
+# sudo cp autostart.sh /etc/init.d/
 # sudo update-rc.d autostart.sh defaults
 
 node /home/georgica/aesthetic.computer/video-sync/server.js &
