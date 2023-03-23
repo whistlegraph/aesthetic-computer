a17ac88fab58045a716b1ce55d1c24e401249abd
Jeffrey Alan Scudder <me@jas.life>
2023-03-23 04:31:35 -0400

    new autostart method using systemd

--- video-sync/autostart-child.sh
+++ video-sync/autostart-child.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# See also: `autostart.sh`
+# Installation:
+# chmod 755 autostart-child.sh
+# cp autostart-child.service /etc/systemd/system/
+# sudo systemctl enable autostart-child.service
+
 chromium-browser --kiosk '/home/georgica/aesthetic.computer/video-sync/index-child.html' 
--- video-sync/autostart.sh
+++ video-sync/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Installation to run this file on boot:
-# chmod +x autostart.sh
-# sudo cp autostart.sh /etc/init.d/
-# sudo update-rc.d autostart.sh defaults
+# Installation:
+# chmod 755 autostart.sh
+# cp autostart.service /etc/systemd/system/
+# sudo systemctl enable autostart.service
 
 node /home/georgica/aesthetic.computer/video-sync/server.js &
 sleep 3
