3a9ea63405621e26fb7fb165a35965b1d42b32d9
Jeffrey Alan Scudder <me@jas.life>
2023-03-23 02:52:49 -0400

    autostart chrome kiosk; restart video logic

---
+++ video-sync/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Installation to run this file on boot:
+# chmod +x autostart.sh
+# cp autostart.sh /etc/init.d/
+# sudo update-rc.d autostart.sh defaults
+
+chromium-browser --kiosk /home/georgica/aesthetic.computer/video-sync/index.html 
--- video-sync/server.js
+++ video-sync/server.js
@@ -1,53 +1,72 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
 const WebSocket = require("ws");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    // Serve the HTML page
-    fs.readFile("index.html", (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end("Error loading index.html");
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/video.mp4") {
-    // Serve the video file
-    const stream = fs.createReadStream("video.mp4");
-    res.writeHead(200, { "Content-Type": "video/mp4" });
-    stream.pipe(res);
-  } else {
-    // 404 error
-    res.writeHead(404);
-    res.end("Page not found");
-  }
-});
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     // Serve the HTML page
+//     fs.readFile("index.html", (err, data) => {
+//       if (err) {
+//         res.writeHead(500);
+//         res.end("Error loading index.html");
+//         return;
+//       }
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.end(data);
+//     });
+//     //} else if (req.url === "/video.mp4") {
+//     // Serve the video file from the RPI boot partition.
+//     //const stream = fs.createReadStream("/boot/screenplay.mp4");
+//     //res.writeHead(200, { "Content-Type": "video/mp4" });
+//     //stream.pipe(res);
+//   } else {
+//     // 404 error
+//     res.writeHead(404);
+//     res.end("Page not found");
+//   }
+// });
 
-server.listen(8000, () => {
-  console.log("Server running on port 8000");
-});
+// server.listen(8000, () => {
+//   console.log("Server running on port 8000");
+// });
 
 const wss = new WebSocket.Server({ port: 8080 });
 
 let clients = [];
+let playing = false;
+let canRestart = true;
 
 wss.on("connection", (ws) => {
   clients.push(ws);
 
   ws.on("message", (message) => {
-    // When a message is received, broadcast it to all clients
-    clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+    if (message === "start" && clients.length > 1) {
+      // Make sure this isn't the first connected client, and then
+      // if this is the first client to start the video,
+      // broadcast a 'play' message to all clients.
+      if (!playing) {
+        playing = true;
+        clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send("play");
+          }
+        });
       }
-    });
+    } else if (message === "restart" && canRestart) {
+      // Broadcast a 'restart' message to all clients to trigger the video playback to start over
+      clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send("restart");
+        }
+      });
+
+      canRestart = false;
+      setTimeout(() => canRestart = true, 500); // Prevent double restarts by adding a .5 second buffer.
+    }
   });
 
   ws.on("close", () => {
-    // Remove the client from the list of clients when the connection is closed
+    // Remove the client from the list of clients when the connection is closed.
     clients = clients.filter((client) => client !== ws);
   });
 });
