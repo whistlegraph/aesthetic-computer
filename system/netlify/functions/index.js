6bd508ae9497abf672bffb44dec2c2f2a941259b
Jeffrey Alan Scudder <me@jas.life>
2023-03-28 13:11:34 -0400

    add cors headers back to index route

--- system/netlify/functions/index.js
+++ system/netlify/functions/index.js
@@ -123,6 +123,8 @@ async function fun(event, context) {
     statusCode: 200,
     headers: {
       "Content-Type": "text/html",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
     },
     body: html,
     ttl: 60,
