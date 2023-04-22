0ad2cca6047ecaafc6501b069eca71235d08c477
Jeffrey Alan Scudder <me@jas.life>
2023-04-22 14:10:38 -0400

    add ordinals notes

---
+++ ordinals/inscribe.sh
@@ -0,0 +1,18 @@
+./ord wallet inscribe --dry-run --satpoint fbd8077ac3294a24423c09aa00974ada3e8cca720b61407e887cb3127d1482d9:0 --fee-rate 4 ~/inscriptions/0.webp 
+
+# Sat calculator: https://instacalc.com/56229
+# Fee determinant: https://instacalc.com/56229 (high priority) 
+#
+# pre-launch wallet test: 1711330103397978
+#
+# 0. first handwritten inscription: 1711330103062117 - "all of computing is about to change" - name: bsoehhdkcvk - 0.webp
+#
+# {
+#  "commit": "39a0a50bab3cae97980cd0cd8fb40e9da8fd479250dfc94a33fc82b050863c2f",
+#  "inscription": "c4218e71c1f82597842b743b5c187646fb4d42ec5b3969cf1616638dddf7fb45i0",
+#  "reveal": "c4218e71c1f82597842b743b5c187646fb4d42ec5b3969cf1616638dddf7fb45",
+#  "fees": 87940
+# }
+#
+# 1. 2nd handwritten inscription: 877482544897754 - "?" - name: hqdekuvdjrz
+# output: 994799b2e22ab95b1f7d0be651e6ed6919b33a05caaeb3077a406acf29d2a5e3:0:0
