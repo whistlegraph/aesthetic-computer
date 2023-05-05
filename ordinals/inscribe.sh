d27a139dba0ef50681c238d0782c8ea52a885578
Jeffrey Alan Scudder <me@jas.life>
2023-05-04 20:45:51 -0400

    ordfish 3

--- ordinals/inscribe.sh
+++ ordinals/inscribe.sh
@@ -5,7 +5,7 @@
 
 ./ord wallet inscribe --dry-run --satpoint ...output:offset... --fee-rate 4 ~/inscriptions/1.webp 
 
-./ord wallet inscribe --dry-run --satpoint 96f7a8968624bb27cc4eabbd2d78a7fdb4aabfcb856b2e72408ad5ec0401f90d:0:0 --fee-rate 4 --destination BC1QULNA34US79KMR2Y7VRKFUY8W7KAVQ53PQ5CFST ~/ordfish/1-afajydklwun.webp 
+./ord wallet inscribe --dry-run --satpoint _ --fee-rate 4 --destination _ ~/ordfish/{number}-{name}.webp 
 
 
 # ordfish
@@ -32,195 +32,203 @@
 #   "fees": 60600
 # }
 
-# 03: 
-#
+# 03: https://ordinals.com/sat/1711330103456693
+#     bsoehhcnrdk
 #     BC1QZ88U8QZFXNXHCNCYG0Z0CACTD89F6M35A34LN8
+# ./ord wallet inscribe --dry-run --fee-rate 120 --satpoint 229914c02aa989fe843ac089006d18f72c6d8fbde9ef9f383cfeaf5b85ba11de:1:0 --destination BC1QZ88U8QZFXNXHCNCYG0Z0CACTD89F6M35A34LN8 ~/ordfish/3-bsoehhcnrdk.webp 
+# {
+#   "commit": "5f6a4aa4007d513d262884cc4592ec73c44a0a163b4bf975e2f0e2f9c08e923b",
+#   "inscription": "ea356f87bda9f6a2a2f63e2a7aa8182bbd43a0e7ffb42a0db6669a255f90dfb8i0",
+#   "reveal": "ea356f87bda9f6a2a2f63e2a7aa8182bbd43a0e7ffb42a0db6669a255f90dfb8",
+#   "fees": 64680
+# }
 
-# 04: 
+
+# 04: https://ordinals.com/sat/
 #
 #     BC1QT6DAFNFRD3YQVM4YL2NX8PAJ6ECG7MMX4R37TQ
 
-# 05: 
+# 05: https://ordinals.com/sat/
 #
 #     BC1QRMEU80S4KGMWWHR6QG889LMGEYSXRMSHYX3WYQ
 
-# 06: 
+# 06: https://ordinals.com/sat/
 #
 #     BC1QRPCNMDW93CUTR2WTG7FLRGNGQEM00XATW3YRFH
 
-# 07:  
+# 07: https://ordinals.com/sat/
 #
 #     BC1Q2D9RZ7XFLUUP4Y8WT4Q08S87Z55GY6PY805RVL
 
-# 08: 
+# 08: https://ordinals.com/sat/ 
 #
 #     BC1Q2QJEMUYGZG78P409GT0U2PSSUQGVA2K8FVAMMN
 
-# 09: 
+# 09: https://ordinals.com/sat/ 
 #
 #     BC1QZMVERNJARQ49K049UPGA5JXSZR4F3S0TKYESY6
 
-# 10:  
+# 10: https://ordinals.com/sat/  
 #
 #     BC1QYL7AJU57SXK2V32ALFXW00LVS4GZ7GQXLAE7EW
 
-# 11: 
+# 11: https://ordinals.com/sat/ 
 #
 #     BC1QFJDHA9KXPRU4GPE934UGN3MDHYWW7Q9QLX2V3D
 
-# 12: 
+# 12: https://ordinals.com/sat/ 
 #
 #     BC1Q2L9KUMKEA9MDMZ6N73G0MFLU59FL2YKUQK7W2U
 
-# 13: 
+# 13: https://ordinals.com/sat/ 
 #
 #     BC1Q2QZJE7DHKTAEHE8CQD42620TJQYFZ6HG28A6MV
 
-# 14: 
+# 14: https://ordinals.com/sat/ 
 #
 #     BC1QGN9WGJ3FGCDH6FLPE687ZJNDT47VMHF9H40XFS
 
-# 15: 
+# 15: https://ordinals.com/sat/ 
 #
 #     BC1Q0QHCTC0WHHFLEL0MY6VRQ2JK3NGER427TYEPFX
 
-# 16: 
+# 16: https://ordinals.com/sat/ 
 #
 #     BC1QQXL7DHUERW56TXFKK30NTHMDKZM3K8QWFNW5X7
 
-# 17: 
+# 17: https://ordinals.com/sat/ 
 #
 #     BC1QKMS3JDLA5GVKXHRYW0AWA46ZVKV9GEYKF6FTQ4
 
-# 18: 
+# 18: https://ordinals.com/sat/ 
 #
 #     BC1QKZQK0K0265UTP72MKPW3CJ70YWE5D2EHZPGW7P
 
-# 19: 
+# 19: https://ordinals.com/sat/ 
 #
 #     BC1QSFLFVHRXAFR4AXMCJ797TXHXZETCHZVMK9M5V8
 
-# 20: 
+# 20: https://ordinals.com/sat/ 
 #
 #     BC1QGRUHWJJZT3FM2VUWYP5DER3T8LP6CLCEWUUUFG
 
-# 21: 
+# 21: https://ordinals.com/sat/ 
 #
 #     BC1QFC3F4R7HKJX639JS4YZZDX930XZ52KGY0TVT5M
 
-# 22: 
+# 22: https://ordinals.com/sat/ 
 #
 #     BC1Q8KJLZGX8VMNVL2UHKK6KJ5ANFAN8532LDEPRJZ
 
-# 23: 
+# 23: https://ordinals.com/sat/ 
 #
 #     BC1Q2DXGNW5W8EDNWEQHW6GNVV2HAADCGT8H30FCRM
 
-# 24: 
+# 24: https://ordinals.com/sat/ 
 #
 #     BC1QA7LY3WVP2FW65TL8AUUSKPLGUA6Y34RKG28CSY
 
-# 25: 
+# 25: https://ordinals.com/sat/ 
 #
 #     BC1QADPNRU9FJEXWTZPMTUNDAD2PUKGVELP7UL6YHR
 
-# 26: 
+# 26: https://ordinals.com/sat/ 
 #
 #     BC1Q49764R4E3UKSS25UZVJWJ4ESF5UV7R38K67CFY
 
-# 27: 
+# 27: https://ordinals.com/sat/ 
 #
 #     BC1QTCFXPTJT6E2XD0EAHC65TXRAGH3QD9RNETZX3T
 
-# 28:  
+# 28: https://ordinals.com/sat/  
 #
 #     BC1Q7QF0Z8D4V5KKQYAHFKG4Q504XTFH6TSXW90ZSS
 
-# 29:  
+# 29: https://ordinals.com/sat/  
 #
 #     BC1Q2EH04L705G5XDZHF4Q5GVQNCY5RRDPQ33K8HWA
 
-# 30:  
+# 30: https://ordinals.com/sat/  
 #
 #     BC1Q0RU0JCRCL0ZUUY8PQG77289ZK0C4XRU3LSKHXZ
 
-# 31:  
+# 31: https://ordinals.com/sat/  
 #
 #     BC1QGT4P0WYPYJWDZ6ESP4K39UHFMGXDCL9T8AHZR4
 
-# 32:  
+# 32: https://ordinals.com/sat/  
 #
 #     BC1QPRKTH3ENK9YTWMYGYCYU6MTGZH2K2DEM3M7KZT
 
-# 33: 
+# 33: https://ordinals.com/sat/ 
 #
 #     BC1QV2JDNCCQKX5L95CSYCM24S02CQNEFTDQESN355
 
-# 34:  
+# 34: https://ordinals.com/sat/  
 #
 #     BC1QJ90Z2JQAAP6HYQ74GTMZEZNNZL87FGGC4CZYTE
 
-# 35:  
+# 35: https://ordinals.com/sat/  
 #
 #     BC1Q644KNZYEESZ03T4U07CQKVVF0544JDJNNZWKLT
 
-# 36:  
+# 36: https://ordinals.com/sat/  
 #
 #     BC1QQUCM5JPPA6S2MJ609RMNJ7Y9AQUDXY593V9QQG
 
-# 37:  
+# 37: https://ordinals.com/sat/  
 #
 #     BC1QQEADCW43SDH3VYRLC2TSLMWM908V88XWSPL3P8
 
-# 38:  
+# 38: https://ordinals.com/sat/  
 #
 #     BC1Q9WCQ634NCNQNP59CAZCKYA4X5TH5D3ETPKPEQP
 
-# 39:  
+# 39: https://ordinals.com/sat/  
 #
 #     BC1QZRF5VPQMHT27N7QKA3Y0QT908HYDPPMFAAP9P5
 
-# 40:  
+# 40: https://ordinals.com/sat/  
 #
 #     BC1QW6AMP5M250XUEU4X32SYDFCSY7385HVYY9YM4Y
 
-# 41:  
+# 41: https://ordinals.com/sat/  
 #
 #     BC1QNA9MS8L6U3DQNZ6983FPGUJ9TG5H8ZT55HCE7V
 
-# 42:  
+# 42: https://ordinals.com/sat/  
 #
 #     BC1QX2QN6V2QQN9S4LW2ZHLWELKVS0JD5METG03HTP
 
-# 43:  
+# 43: https://ordinals.com/sat/  
 #
 #     BC1QV6QZTV0SLA2C54YN8T5LXPD0643FDWS22TQP5C
 
-# 44:  
+# 44: https://ordinals.com/sat/  
 #
 #     BC07U82ZV9XDMW83E8QZHH7J68KC70G9UQJMNNRTPA
 
-# 45:  
+# 45: https://ordinals.com/sat/  
 #
 #     BC1QJ9QU7T9FYTW0HENXDCRCLRW6EKK4EVH2375AK6
 
-# 46:  
+# 46: https://ordinals.com/sat/  
 #
 #     BC1QSNQ9SH3G996WHWC7N88L3CFUZX4LUK9D7368HA
 
-# 47:  
+# 47: https://ordinals.com/sat/  
 #
 #     BC1Q9EXYRF5UFRRCNCD7AXZY3TC597D7JG325QPMC4
 
-# 48:  
+# 48: https://ordinals.com/sat/  
 #
 #     BC1QFG639JCHSYRTYCCF8W2XHY057CMTXVFEFFW4KC
 
-# 49:  
+# 49: https://ordinals.com/sat/  
 #
 #     BC1OXNVTYPNR756K324D0M7PJQHP2XJ0U7GFYPHGDC
 
-# 50:  
+# 50: https://ordinals.com/sat/  
 #
 #     BC1QN53AK6T6YJSTYT5R3PH5L3FRZZWX69C0WYLPAV
 
