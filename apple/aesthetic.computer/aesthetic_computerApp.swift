69ae6183418ceaebfb04fba6be1444d86182b054
Jeffrey Alan Scudder <me@jas.life>
2023-05-26 23:47:25 -0400

    add back xcode project (with a good gitignore)

---
+++ apple/aesthetic.computer/aesthetic_computerApp.swift
@@ -0,0 +1,17 @@
+//
+//  aesthetic_computerApp.swift
+//  aesthetic.computer
+//
+//  Created by Jeffrey Alan Scudder on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aesthetic_computerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
