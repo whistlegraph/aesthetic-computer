a0bd7b0f24bd4b275cc913c88cd6a2dcdcb88b65
Jeffrey Alan Scudder <me@jas.life>
2023-07-12 00:53:24 -0400

    don't use wss.clients Set

--- system/public/aesthetic.computer/lib/disk.mjs
+++ system/public/aesthetic.computer/lib/disk.mjs
@@ -1697,7 +1697,7 @@ async function load(
   // Start the socket server
   // TODO: Before we load the disk, in case of needing to reload remotely on failure? 23.01.27.12.48
   let receiver; // Handles incoming messages from the socket.
-  const forceProd = true; // For testing prod socket servers in development.
+  const forceProd = false; // For testing prod socket servers in development.
 
   // Requests a session-backend and connects via websockets.
   function startSocket() {
@@ -1714,9 +1714,6 @@ async function load(
           (id, type, content) => {
             // Globally receivable messages...
             // (There are also some messages handled in `Socket`)
-
-            console.log(type);
-
             // 😱 Scream at everyone who is connected!
             if (type === "scream") {
               console.log("😱 Scream:", content, "❗");
