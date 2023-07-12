7fce7def74ad4f5e4e1dcef3573c7868011ffcba
Jeffrey Alan Scudder <me@jas.life>
2023-07-12 01:08:18 -0400

    ping pong socket check

--- system/public/aesthetic.computer/lib/socket.mjs
+++ system/public/aesthetic.computer/lib/socket.mjs
@@ -6,6 +6,33 @@
 - [x] Queue any sent messages to be received when the connection opens?
 #endregion */
 
+/*
+Ping Ping Check
+
+import WebSocket from 'ws';
+
+function heartbeat() {
+  clearTimeout(this.pingTimeout);
+
+  // Use `WebSocket#terminate()`, which immediately destroys the connection,
+  // instead of `WebSocket#close()`, which waits for the close timer.
+  // Delay should be equal to the interval at which your server
+  // sends out pings plus a conservative assumption of the latency.
+  this.pingTimeout = setTimeout(() => {
+    this.terminate();
+  }, 30000 + 1000);
+}
+
+const client = new WebSocket('wss://websocket-echo.com/');
+
+client.on('error', console.error);
+client.on('open', heartbeat);
+client.on('ping', heartbeat);
+client.on('close', function clear() {
+  clearTimeout(this.pingTimeout);
+});
+*/
+
 export class Socket {
   id; // Will be filled in with the user identifier after the first message.
   #debug;
