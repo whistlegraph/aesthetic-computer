94f2292230c621e52e3374404d42af393f576281
Jeffrey Alan Scudder <me@jas.life>
2023-09-01 19:13:17 -0400

    inline `corsHeaders` in `say`

--- edge-stream/api/say.js
+++ edge-stream/api/say.js
@@ -11,10 +11,7 @@
   - [x] Get everything working locally.
 #endregion */
 
-import { TextToSpeechClient } from "@google-cloud/text-to-speech";
-import { corsHeaders } from "../help.mjs";
-
-const dev = process.env.VERCEL_ENV === "development";
+const tts = require("@google-cloud/text-to-speech");
 const gcpKey = process.env.GCP_TTS_KEY;
 const gcpEmail = process.env.GCP_EMAIL;
 
@@ -22,7 +19,7 @@ export default async function handler(req, res) {
   const { method, query, headers } = req;
 
   if (method === "GET") {
-    const client = new TextToSpeechClient({
+    const client = new tts.TextToSpeechClient({
       credentials: {
         private_key: gcpKey.replace(/\\n/g, "\n"),
         client_email: gcpEmail,
@@ -30,7 +27,7 @@ export default async function handler(req, res) {
     });
 
     let from = "aesthetic.computer";
-    if (query.from) from = Buffer.from(query.from, 'base64').toString();
+    if (query.from) from = Buffer.from(query.from, "base64").toString();
     const utterance = from || "aesthetic.computer";
     const set = parseInt(query.voice?.split(":")[1]) || 0;
     const gender = query.voice?.split(":")[0]?.toUpperCase() || "NEUTRAL";
@@ -72,23 +69,37 @@ export default async function handler(req, res) {
     if (utterance.indexOf("<speak>") !== -1) ssml = true;
     ttsRequest.input = ssml ? { ssml: utterance } : { text: utterance };
 
-    client.synthesizeSpeech(ttsRequest).then((ttsResponse) => {
-      const audioContent = ttsResponse[0].audioContent;
-
-      console.log(audioContent);
-
-      res.setHeader("Content-Type", "audio/mpeg");
-      res.setHeader("Content-Disposition", 'inline; filename="response.mp3"');
-      for (const [k, v] of Object.entries(corsHeaders(req))) {
-        res.setHeader(k, v);
-      } 
-      res.status(200).send(audioContent);
-    }).catch((err) => {
-      console.log(err)
-      res.status(500).json({message: "An error has occured."});
-    });
-
+    client
+      .synthesizeSpeech(ttsRequest)
+      .then((ttsResponse) => {
+        const audioContent = ttsResponse[0].audioContent;
+        res.setHeader("Content-Type", "audio/mpeg");
+        res.setHeader("Content-Disposition", 'inline; filename="response.mp3"');
+        for (const [k, v] of Object.entries(corsHeaders(req))) {
+          res.setHeader(k, v);
+        }
+        res.status(200).send(audioContent);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "An error has occured." });
+      });
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
   }
+}
+
+// See also: `help.mjs`
+// TODO: Could not import like in `ask.ts` because of the JS environment.
+//       23.09.01.19.11
+function corsHeaders(request) {
+  const dev = process.env.VERCEL_ENV === "development";
+  const production = !dev;
+  const allowedOrigin = production ? "https://aesthetic.computer" : "*";
+
+  return {
+    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    "Access-Control-Allow-Origin": allowedOrigin,
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  }; // Define CORS headers.
 }
