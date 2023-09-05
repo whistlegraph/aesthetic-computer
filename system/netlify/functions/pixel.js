2e1f2df3a96c884549f5c3c1f7bf2262b81e4754
Jeffrey Alan Scudder <me@jas.life>
2023-09-05 02:45:34 -0400

    write custom sticker mockup compositor in `api/pixel`

--- system/netlify/functions/pixel.js
+++ system/netlify/functions/pixel.js
@@ -11,6 +11,7 @@
 
 /* #region 🏁 TODO 
   + Done
+  - [x] Add a compositor for stickers that would be faster than Printful.
   - [x] Add a fitMode.
   - [x] Nearest neighbor scale a painting after opening it.
 #endregion */
@@ -27,8 +28,8 @@ async function fun(event, context) {
     (event.headers["host"] === "aesthetic.computer" || dev)
   ) {
     const params = event.path.replace("/api/pixel/", "").split("/");
-    let [pre, fit] = params[0].split(":");
-    fit ||= "fill"; // or "fill"
+    let [pre, mode] = params[0].split(":");
+    mode ||= "fill"; // or "fill", or "sticker"
     const resolution = pre.split("x").map((n) => parseInt(n));
     const slug = params.slice(1).join("/");
     const imageUrl = `https://${domain}/media/${slug}`;
@@ -42,24 +43,92 @@ async function fun(event, context) {
 
       // Resize the image using nearest neighbor filtering with "sharp"
       // Docs: https://sharp.pixelplumbing.com/api-resize
-      const resizedBuffer = await sharp(response.body)
-        .resize({
-          width: resolution[0],
-          height: resolution[1] || resolution[0],
-          fit, // or "fill" to stretch it
-          kernel: sharp.kernel.nearest,
-          background: { r: 0, g: 0, b: 0, alpha: 0 },
-        })
-        .png()
-        .toBuffer();
+      let buffer;
+
+      const metadata = await sharp(response.body).metadata();
+      const width = resolution[0];
+      const height = resolution[1] || resolution[0];
+      const kernel =
+        width > metadata.width || height > metadata.height
+          ? sharp.kernel.nearest // For scaling up.
+          : sharp.kernel.lanczos3; // For scaling down.
+
+      if (mode !== "sticker") {
+        // A. Simple resizing.
+        buffer = await sharp(response.body)
+          .resize({
+            width,
+            height,
+            fit: mode, // "contain" or "fill"
+            kernel,
+            background: { r: 0, g: 0, b: 0, alpha: 0 },
+          })
+          .png()
+          .toBuffer();
+      } else if (mode === "sticker") {
+        // B. Complex sticker mockup.
+        const scalingFactor =
+          metadata.width > metadata.height
+            ? width / metadata.width
+            : height / metadata.height;
+
+        const margin = 128;
+        const rectWidth = Math.floor(metadata.width * scalingFactor) - margin;
+        const rectHeight = Math.floor(metadata.height * scalingFactor) - margin;
+
+        const resizedBuffer = await sharp(response.body)
+          .resize({
+            width: rectWidth, // Adjusting the target dimensions for the padding
+            height: rectHeight,
+            fit: "fill",
+            kernel,
+            background: { r: 0, g: 0, b: 0, alpha: 0 },
+          })
+          .png()
+          .toBuffer();
+
+        const radius = 24,
+          pad = 48;
+
+        const svg = `
+          <svg width="${rectWidth + margin}" height="${
+            rectHeight + margin
+          }" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <filter id="dropshadow" height="130%">
+                  <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
+                  <feOffset dx="2" dy="2" result="offsetblur"/>
+                  <feComponentTransfer>
+                    <feFuncA type="linear" slope="0.5"/>
+                  </feComponentTransfer>
+                  <feMerge> 
+                    <feMergeNode/> 
+                    <feMergeNode in="SourceGraphic"/> 
+                  </feMerge>
+                </filter>
+              </defs>
+              <rect x="${margin / 2 - pad / 2}" y="${
+                margin / 2 - pad / 2
+              }" width="${rectWidth + pad}" height="${
+                rectHeight + pad
+              }" rx="${radius}" ry="${radius}" fill="white" filter="url(#dropshadow)" />
+          </svg>`;
+
+        const rectangleBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
+
+        // Composite the resized rectangle to the rounded sheet.
+        buffer = await sharp(rectangleBuffer)
+          .composite([{ input: resizedBuffer, blend: "over" }])
+          .toBuffer();
+      }
 
       return {
         statusCode: 200,
         headers: {
           "Content-Type": "image/png",
-          "Content-Length": resizedBuffer.length.toString(),
+          "Content-Length": buffer.length.toString(),
         },
-        body: resizedBuffer.toString("base64"),
+        body: buffer.toString("base64"),
         ttl: 60,
         isBase64Encoded: true,
       };
@@ -75,3 +144,11 @@ async function fun(event, context) {
 }
 
 export const handler = builder(fun);
+
+async function createCenteredRoundedRectangle(
+  bufferWidth,
+  bufferHeight,
+  rectWidth,
+  rectHeight,
+  borderRadius,
+) {}
