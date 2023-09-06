2e6f0a0f2290b072a913cc14d934cedeec1a17ee
Jeffrey Alan Scudder <me@jas.life>
2023-09-05 23:17:27 -0400

    more options for `logo.aesthetic.computer`

--- system/backend/logo.mjs
+++ system/backend/logo.mjs
@@ -1,5 +1,5 @@
 // Logo, 23.09.05.22.31
-// Retrieve / proxy a logo url from aesthetic.computer.
+// Retrieve / proxy a random logo url from the aesthetic.computer logo store.
 
 const slugs = [
   "painting-2023.8.01.17.07.png",
@@ -8,6 +8,12 @@ const slugs = [
   "painting-2023.8.01.14.52.png",
   "painting-2023.8.21.10.19.png",
   "painting-2023.8.01.14.04.png",
+  "painting-2023.8.21.10.50.png",
+  "painting-2023.8.21.10.31.png",
+  "painting-2023.8.01.17.35.png",
+  "painting-2023.8.01.14.33.png",
+  "painting-2023.7.31.15.33.png",
+  "painting-2023.7.29.20.39.png",
 ];
 
 export function logoUrl() {
