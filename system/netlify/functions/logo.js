04e33cb93dd8b403a4c15bc76f6eea1c7c54e70a
Jeffrey Alan Scudder <me@jas.life>
2023-09-05 23:31:25 -0400

    better logo page

--- system/netlify/functions/logo.js
+++ system/netlify/functions/logo.js
@@ -4,25 +4,79 @@
 import { respond } from "../../backend/http.mjs";
 import { logoUrl } from "../../backend/logo.mjs";
 
-// GET A user's `sub` id from either their handle or email address.
 export async function handler(event, context) {
   // Make sure this is a GET request
   if (event.httpMethod !== "GET") {
     return respond(405, { error: "Wrong request type." });
-  } else {
-    const { got } = await import("got");
-    const response = await got(logoUrl(), {
-      responseType: "buffer",
-      https: { rejectUnauthorized: false },
-    });
+  }
 
+  const { got } = await import("got");
+  const response = await got(logoUrl(), {
+    responseType: "buffer",
+    https: { rejectUnauthorized: false },
+  });
+
+  // Check the User-Agent to determine the type of request
+  const userAgent = event.headers["user-agent"] || "";
+  const isServer =
+    userAgent.includes("curl") ||
+    userAgent.includes("wget") ||
+    userAgent.includes("python-requests") ||
+    userAgent.includes("node-fetch");
+
+  if (isServer) {
+    // If it's a server tool or library, return the PNG
     return {
       statusCode: 200,
       headers: {
-        "Content-Type": "image/png", // Adjust this if your image type changes
+        "Content-Type": "image/png",
       },
       body: Buffer.from(response.body, "binary").toString("base64"),
       isBase64Encoded: true,
     };
+  } else {
+    // If it's not recognized as a server tool, return HTML with the image centered and background purple
+    const htmlResponse = `
+      <html>
+        <head>
+          <link rel="icon" href="${logoUrl()}" type="image/x-icon">
+          <style>
+            body { 
+              display: flex; 
+              justify-content: center; 
+              align-items: center; 
+              height: 100vh;
+              background-color: ${randomPurple()};
+              margin: 0;
+              overflow: hidden;
+            }
+            img { 
+              object-fit: contain;
+              width: 100vw;
+              height: 100vh;
+              cursor: pointer;
+            }
+          </style>
+        </head>
+        <body>
+          <img crossorigin src="${logoUrl()}" onclick="location.reload()">
+        </body>
+      </html>
+    `;
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+      body: htmlResponse,
+    };
   }
 }
+
+const randomPurple = () => {
+  const r = Math.floor(Math.random() * 50 + 50); // Values between 200 and 255
+  const g = Math.floor(Math.random() * 25); // Values between 0 and 50
+  const b = Math.floor(Math.random() * 50 + 50); // Values between 200 and 255
+  return `rgb(${r},${g},${b})`;
+};
