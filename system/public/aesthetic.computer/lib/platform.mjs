c473a2e114f4765968e73d4c072629c14a1c3f8b
Jeffrey Alan Scudder <me@jas.life>
2023-09-08 20:30:09 -0400

    insert proper tiktok useragent

--- system/public/aesthetic.computer/lib/platform.mjs
+++ system/public/aesthetic.computer/lib/platform.mjs
@@ -12,4 +12,4 @@ export const Android = /(Android)/g.test(nav.userAgent);
 export const MetaBrowser = /(OculusBrowser)/g.test(nav.userAgent);
 export const Desktop = !iOS && !Android && !MetaBrowser;
 export const Instagram = /(Instagram)/g.test(nav.userAgent);
-export const TikTok = /TikTok/i.test(navigator.userAgent);
+export const TikTok = /BytedanceWebview/i.test(navigator.userAgent);
