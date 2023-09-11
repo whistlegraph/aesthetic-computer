fc58f885cb9a4b69e0600b5abe2c282ef7d80122
Jeffrey Alan Scudder <me@jas.life>
2023-09-11 01:47:01 -0400

    auto center text for `word` command (on multiple lines)

--- system/public/aesthetic.computer/lib/disk.mjs
+++ system/public/aesthetic.computer/lib/disk.mjs
@@ -1298,7 +1298,7 @@ const $paintApi = {
       // Center multi-line text. (Kinda hacky? 23.08.22.20.30)
       if (lines.length > 1 && pos.center.indexOf("y") !== -1) {
         const blockHeight = 11;
-        pos.y = screen.height / 2 - (lines.length * blockHeight) / 4;
+        pos.y = pos.y - (lines.length * blockHeight) / 2 + blockHeight / 2;
       }
 
       lines.forEach((line, index) => {
