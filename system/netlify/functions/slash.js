5c60192d9cdff79d39db4d584680c89925c0d608
Jeffrey Alan Scudder <me@jas.life>
2023-10-13 02:36:45 -0400

    better verification in `slash`

--- system/netlify/functions/slash.js
+++ system/netlify/functions/slash.js
@@ -2,7 +2,7 @@
 // A helpful Discord webhook for auto-linking AC pieces.
 
 import { respond } from "../../backend/http.mjs";
-import { verifyKey } from "discord-interactions";
+import { verifyKey, InteractionResponseType } from "discord-interactions";
 
 export async function handler(event) {
   const timestamp = event.headers["X-Signature-Timestamp"];
@@ -12,21 +12,18 @@ export async function handler(event) {
     return respond(500, { message: "😈 Unauthorized." });
 
   const isValidRequest = verifyKey(
-    buf,
+    Buffer.from(event.body, "utf8"),
     signature,
     timestamp,
     process.env.DISCORD_PAL_PUBLIC,
   );
 
   if (!isValidRequest) {
-    return respond(401, {
-      type: 4,
-      data: { content: "😫 Invalid request signature." },
-    });
+    return respond(401, { message: "😫 Invalid request signature." });
   }
 
   return respond(200, {
-    type: 4, // a message response
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: { content: "hello world 🗺️" },
   });
 }
