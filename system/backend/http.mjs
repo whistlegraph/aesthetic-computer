2356deea7cb8f20f656a314d3d0079255b3f0a0a
Jeffrey Alan Scudder <me@jas.life>
2023-10-13 15:55:02 -0400

    add content-type json to `slash` (and all of `respond`)

--- system/backend/http.mjs
+++ system/backend/http.mjs
@@ -2,7 +2,8 @@
 // Helpers for making backend functions more nicely
 // written.
 
-function respond(statusCode, body, headers) {
+function respond(statusCode, body, headers = {}) {
+  headers["Content-Type"] = "application/json"; // Always return a JSON reply.
   const res = { statusCode, headers };
   if (body) res.body = JSON.stringify(body);
   return res;
