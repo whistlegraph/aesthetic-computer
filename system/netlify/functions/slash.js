bf4c41116cd3ad07fe4e9fa0bcf3e9a2816b436f
Jeffrey Alan Scudder <me@jas.life>
2023-10-13 02:24:59 -0400

    use `discord-interactions` in `slash`

--- system/netlify/functions/slash.js
+++ system/netlify/functions/slash.js
@@ -1,22 +1,25 @@
 // Slash, 23.10.13.00.08
 // A helpful Discord webhook for auto-linking AC pieces.
 
-import { createHmac } from "crypto";
 import { respond } from "../../backend/http.mjs";
+import { verifyKey } from "discord-interactions";
 
 export async function handler(event) {
   const timestamp = event.headers["X-Signature-Timestamp"];
-  const discordSignature = event.headers["X-Signature-Ed25519"];
+  const signature = event.headers["X-Signature-Ed25519"];
 
-  if (!timestamp && !discordSignature)
+  if (!timestamp && !signature)
     return respond(500, { message: "😈 Unauthorized." });
 
-  const signature = createHmac("sha256", process.env.DISCORD_PAL_PUBLIC)
-    .update(timestamp + (await event.text()))
-    .digest("hex");
+  const isValidRequest = verifyKey(
+    buf,
+    signature,
+    timestamp,
+    process.env.DISCORD_PAL_PUBLIC,
+  );
 
-  if (discordSignature !== signature) {
-    return respond(200, {
+  if (!isValidRequest) {
+    return respond(401, {
       type: 4,
       data: { content: "😫 Invalid request signature." },
     });
