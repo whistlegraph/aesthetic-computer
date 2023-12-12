3642bd084f6e015988277ba55c581267a43da00d
ttarigh <99256332+ttarigh@users.noreply.github.com>
2023-12-12 15:51:30 -0500

    App Release Candidate

--- apple/aesthetic.computer/ContentView.swift
+++ apple/aesthetic.computer/ContentView.swift
@@ -8,92 +8,135 @@ class Coordinator: NSObject, WKScriptMessageHandler {
     // Handle JavaScript messages here
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if message.name == "iOSAppLog" {
-                print("JavaScript Log: \(message.body)")
-            } 
-    }
-}
-
-struct WebView: UIViewRepresentable {
-    var url: String
-    var isOnline: Bool
-    
-    func makeCoordinator() -> Coordinator {
-        return Coordinator()
-    }
-    
-    func makeUIView(context: Context) -> WKWebView {
-        let config = WKWebViewConfiguration()
-        let userScript = WKUserScript(source: "console.log = function() { window.webkit.messageHandlers.iOSAppLog.postMessage([...arguments].join(' ')); }",
-                                      injectionTime: .atDocumentStart,
-                                      forMainFrameOnly: false)
-        config.allowsInlineMediaPlayback = true
-        config.userContentController.addUserScript(userScript)
-        config.userContentController.add(context.coordinator, name: "iOSAppLog")
-        config.userContentController.add(context.coordinator, name: "iOSApp")
-        let webView = WKWebView(frame: .zero, configuration: config)
-        webView.backgroundColor = UIColor(red: grey, green: grey, blue: grey, alpha: 1)
-        webView.isOpaque = false
-        
-        webView.customUserAgent = "Aesthetic"
-        
-        
-        // Add a script message handler to handle messages from JavaScript
-        AppDelegate.shared?.appWebView = webView // Set the shared appWebView
-        return webView
-    }
-    
-    func updateUIView(_ webView: WKWebView, context: Context) {
-        print("URL: ", url)
-        let request = URLRequest(url: URL(string: url)!)
-        webView.load(request)
-        
-        // Call JavaScript function if online
-        if isOnline {
-            let script = "shareImage();" // Assuming the JavaScript function is named 'shareImage'
-            webView.evaluateJavaScript(script, completionHandler: nil)
+            print("JavaScript Log: \(message.body)")
         }
-    }
-}
-
-struct ContentView: View {
-    @State private var isOnline: Bool? = nil
-    
-    var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                if let isOnline = isOnline {
-                    if isOnline {
-                        WebView(url: "https://aesthetic.computer", isOnline: true)
-                    } else {
-                        let test = (Bundle.main.url(forResource: "offline", withExtension: "html", subdirectory: "html")?.absoluteString ?? "")
-                        WebView(url: test, isOnline: false)
+        else if message.name == "iOSApp", let jsonString = message.body as? String {
+            
+            // Convert JSON string to Dictionary
+            print("JavaScript Log: \(message.body)")
+            if let data = jsonString.data(using: .utf8) {
+                do {
+                    if let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        // Now 'dictionary' is a Swift dictionary
+                        print("JSON as dictionary: \(dictionary)")
                         
+                        // Handle the dictionary as needed
+                        if let type = dictionary["type"] as? String, type == "notifications" {
+                            if let body = dictionary["body"] as? Bool, body == true{
+                                AppDelegate.shared?.triggerSubscribe()
+                                showAlert(title: "SUBSCRIBED", message: "You have successfully subscribed to notifications. Type \"nonotifs\" to unsubscribe.")
+                            }
+                            else if let body = dictionary["body"] as? Bool, body == false{
+                                AppDelegate.shared?.triggerUnsubscribe()
+                                showAlert(title: "UNSUBSCRIBED", message: "You have successfully unsubscribed to notifications. Type \"notifs\" to subscribe.")
+                            }
+                        }
                     }
-                } else {
-                    Color(red: grey, green: grey, blue: grey)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                } catch {
+                    print("Error parsing JSON: \(error)")
                 }
             }
-            .onAppear {
-                let monitor = NWPathMonitor()
-                monitor.pathUpdateHandler = { path in
-                    self.isOnline = path.status == .satisfied
-                    if self.isOnline == true {
-                        monitor.cancel() //stops monitoring once you are online
-                    }
-                }
-                let queue = DispatchQueue(label: "NetworkMonitor")
-                monitor.start(queue: queue)
+        }
+    }
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+        // Find the active window scene
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            
+            // Present the alert from the top-most view controller
+            var currentController = rootViewController
+            while let presentedController = currentController.presentedViewController {
+                currentController = presentedController
             }
-            .padding(.bottom, geometry.safeAreaInsets.bottom > 0 ? 24 : 0)
-            .background(Color(red: grey, green: grey, blue: grey))
-            .ignoresSafeArea(.keyboard, edges: .bottom)
+            currentController.present(alert, animated: true, completion: nil)
         }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct WebView: UIViewRepresentable {
+        var url: String
+        var isOnline: Bool
+        
+        func makeCoordinator() -> Coordinator {
+            return Coordinator()
+        }
+        
+        func makeUIView(context: Context) -> WKWebView {
+            let config = WKWebViewConfiguration()
+            let userScript = WKUserScript(source: "console.log = function() { window.webkit.messageHandlers.iOSAppLog.postMessage([...arguments].join(' ')); }",
+                                          injectionTime: .atDocumentStart,
+                                          forMainFrameOnly: false)
+            config.allowsInlineMediaPlayback = true
+            config.userContentController.addUserScript(userScript)
+            config.userContentController.add(context.coordinator, name: "iOSAppLog")
+            config.userContentController.add(context.coordinator, name: "iOSApp")
+            let webView = WKWebView(frame: .zero, configuration: config)
+            webView.backgroundColor = UIColor(red: grey, green: grey, blue: grey, alpha: 1)
+            webView.isOpaque = false
+            
+            webView.customUserAgent = "Aesthetic"
+            
+            
+            // Add a script message handler to handle messages from JavaScript
+            AppDelegate.shared?.appWebView = webView // Set the shared appWebView
+            return webView
+        }
+        
+        func updateUIView(_ webView: WKWebView, context: Context) {
+            print("URL: ", url)
+            let request = URLRequest(url: URL(string: url)!)
+            webView.load(request)
+            
+            // Call JavaScript function if online
+            if isOnline {
+                let script = "shareImage();" // Assuming the JavaScript function is named 'shareImage'
+                webView.evaluateJavaScript(script, completionHandler: nil)
+            }
+        }
     }
-}
+    
+    struct ContentView: View {
+        @State private var isOnline: Bool? = nil
+        
+        var body: some View {
+            GeometryReader { geometry in
+                VStack {
+                    if let isOnline = isOnline {
+                        if isOnline {
+                            WebView(url: "https://aesthetic.computer", isOnline: true)
+                        } else {
+                            let test = (Bundle.main.url(forResource: "offline", withExtension: "html", subdirectory: "html")?.absoluteString ?? "")
+                            WebView(url: test, isOnline: false)
+                            
+                        }
+                    } else {
+                        Color(red: grey, green: grey, blue: grey)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+                }
+                .onAppear {
+                    let monitor = NWPathMonitor()
+                    monitor.pathUpdateHandler = { path in
+                        self.isOnline = path.status == .satisfied
+                        if self.isOnline == true {
+                            monitor.cancel() //stops monitoring once you are online
+                        }
+                    }
+                    let queue = DispatchQueue(label: "NetworkMonitor")
+                    monitor.start(queue: queue)
+                }
+                .padding(.bottom, geometry.safeAreaInsets.bottom > 0 ? 24 : 0)
+                .background(Color(red: grey, green: grey, blue: grey))
+                .ignoresSafeArea(.keyboard, edges: .bottom)
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
