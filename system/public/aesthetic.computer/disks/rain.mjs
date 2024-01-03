7ff6eff8bb2501a2428746f1dc7050c6ad2aa72d
Jeffrey Alan Scudder <me@jas.life>
2024-01-03 21:39:34 +0100

    add `rain` for aspen

--- system/public/aesthetic.computer/disks/rain.mjs
+++ system/public/aesthetic.computer/disks/rain.mjs
@@ -1,32 +1,61 @@
 // Rain, 2023.12.21.17.00.56.185
-// Rain is simply falling on someone.
+// Rain is simply falling on someone, drawn by Aspen.
 
 /* #region 📚 README 
 #endregion */
 
 /* #region 🏁 TODO 
+  - [] Add the song notes for "rain, rain, go away".
+  + Done
+  - [x] Add Aspen's graphic.
+  - [x] Add rain.
 #endregion */
 
+let drops = [],
+  guy;
+
 // 🥾 Boot
-function boot({ api, wipe }) {
+function boot({ api, wipe, net: { preload } }) {
   // Runs once at the start.
-  wipe("gray"); // Clear's the screen. Can use R, G, B or CSS colors.
+  preload(`/assets/rain/aspens-rain-guy-nobg.png`).then(
+    ({ img }) => (guy = img),
+  );
+}
+
+// 🧮 Sim
+function sim({ simCount, screen, num, help: { repeat, choose } }) {
+  // Generate drops.
+  //if (simCount % 16n === 0n) {
+  repeat(num.randIntRange(1, 16), () => {
+    drops.push({
+      x: num.randInt(screen.width),
+      y: 0,
+      speed: num.randIntRange(5, 10),
+      state: "alive",
+      color: choose("blue", "teal"),
+    });
+  });
+  //}
+
+  drops.forEach((drop) => {
+    drop.y += drop.speed * 0.35; // Make drops fall.
+    if (drop.y >= screen.height) drop.state = "dead";
+  });
+
+  drops = drops.filter((drop) => drop.state !== "dead"); // Remove dead drops.
 }
 
 // 🎨 Paint
-function paint({ api, ink, line, pen, box }) {
-  // ink("red").line(0, 0, 100, 100); Would draw a diagonal line.
-  // return false; // Uncomment for proce55ing's "noLoop" functionality.
+function paint({ wipe, ink, screen, paste }) {
+  wipe("darkblue"); // Clear's the screen. Can use R, G, B or CSS colors.
+  ink("green").box(0, screen.height - 20, screen.width);
+  paste(guy, screen.width / 2 - guy.width / 2, screen.height - guy.height);
+  drops.forEach((drop) => ink(drop.color).point(drop));
 }
 
 // 🎪 Act
 // function act({ event: e }) {
 //  // Respond to user input here.
-// }
-
-// 🧮 Sim
-// function sim() {
-//  // Runs once per logic frame. (120fps locked.)
 // }
 
 // 🥁 Beat
@@ -57,7 +86,7 @@ function meta() {
 // Render an application icon, aka favicon.
 // }
 
-export { boot, paint, meta };
+export { boot, sim, paint, meta };
 
 // 📚 Library
 //   (Useful functions used throughout the piece)
