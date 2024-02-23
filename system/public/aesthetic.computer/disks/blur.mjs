c2cd8f2f40ba55b23af8108f76eaab90b0128c59
Miles Peyton <milespeyton@gmail.com>
2024-02-23 16:27:13 -0500

    randomize pixels to blur

--- system/public/aesthetic.computer/disks/blur.mjs
+++ system/public/aesthetic.computer/disks/blur.mjs
@@ -22,6 +22,8 @@ function paint({ ink, pen, params, page, pixel, screen, system }) {
   }
   const nopaint = system.nopaint;
 
+  const numBlurSamples = Math.max(128, radius * 8);
+
   if (nopaint.is("painting")) {
     page(screen);
     ink(255, 0, 0).circle(pen.x, pen.y, radius); // Circle overlay.
@@ -30,32 +32,32 @@ function paint({ ink, pen, params, page, pixel, screen, system }) {
     const brush = nopaint.brush;
     const blurRad = 2;
 
-    for (let x = -radius; x < radius; x++) {
-      for (let y = -radius; y < radius; y++) {
-        if (x * x + y * y < radius * radius) {
-          const xy = [brush.x + x, brush.y + y];
-          let avgCols = [0, 0, 0, 0];
+    let srcColor = pixel(brush.x, brush.y, system.painting);
 
-          let numSamples = 0;
+    for (let i = 0; i < numBlurSamples; i++) {
+      const randomX = Math.floor(Math.random() * (2 * radius)) - radius;
+      const randomY = Math.floor(Math.random() * (2 * radius)) - radius;
+      const xy = [brush.x + randomX, brush.y + randomY];
+      let avgCol = [0, 0, 0, 0];
 
-          for (let bx = -blurRad; bx < blurRad; bx++) {
-            for (let by = -blurRad; by < blurRad; by++) {
-              if (bx * bx + by * by < blurRad * blurRad) {
-                const sampleXY = [xy[0] + bx, xy[1] + by];
-                const sampleCol = pixel(...sampleXY, system.painting);
-                for (let i = 0; i < 4; i++) {
-                  avgCols[i] += sampleCol[i];
-                }
-                numSamples++;
-              }
+      let numSamples = 0;
+
+      for (let bx = -blurRad; bx < blurRad; bx++) {
+        for (let by = -blurRad; by < blurRad; by++) {
+          if (bx * bx + by * by < blurRad * blurRad) {
+            const sampleXY = [xy[0] + bx, xy[1] + by];
+            const sampleCol = pixel(...sampleXY, system.painting);
+            for (let i = 0; i < 4; i++) {
+              avgCol[i] += sampleCol[i];
             }
+            numSamples++;
           }
-
-          for (let i = 0; i < 4; i++) avgCols[i] /= numSamples;
-          avgCols[3] = 255;
-          ink(...avgCols).point(...xy);
         }
       }
+
+      for (let i = 0; i < 4; i++) avgCol[i] /= numSamples;
+      avgCol[3] = 255;
+      ink(...avgCol).point(...xy);
     }
   }
 }
