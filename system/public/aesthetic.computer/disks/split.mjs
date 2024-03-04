8dde3dc53766a880fb70ca945f1b458becff61f9
Jeffrey Alan Scudder <me@jas.life>
2024-03-03 16:03:57 -0800

    scaffold out `split` (wip) for developing world persistence

---
+++ system/public/aesthetic.computer/disks/split.mjs
@@ -0,0 +1,48 @@
+// Split, 2024.3.03.15.46.29.905
+// Run two instances of aesthetic computer inside itself, side by side.
+
+/* #region 📚 README 
+#endregion */
+
+/* #region 🏁 TODO 
+  - [-] Make it so that `split` has an escape of some kind?
+  - [] Take care of `gap` spacing.
+#endregion */
+
+// export const nohud = true;
+
+function boot({ params, dom: { html }, resolution, screen }) {
+  resolution(screen.width, screen.height, 0);
+  html`
+    <style>
+      #content {
+        display; flex;
+        flex-direction: column;
+        padding-top: 2.75em;
+        box-sizing: border-box;
+      }
+      iframe {
+        box-sizing: border-box;
+        width: 100%;
+        height: 50%;
+        border: none;
+        background: black;
+      }
+    </style>
+    <iframe src="/${params[0] || ''}"></iframe>
+    <iframe src="/${params[0] || params[1] || ''}"></iframe>
+  `;
+}
+
+// 🎨 Paint
+function paint({ wipe }) {
+  wipe("gray");
+  return false;
+}
+
+// 🎪 Act
+// function act({ event: e }) {
+//  // Respond to user input here.
+// }
+
+export { boot, paint };
