39f7134f45c0d1d77651d9959fe97cfb8642546c
Jeffrey Alan Scudder <me@jas.life>
2024-03-06 23:19:09 -0800

    more emacs lisp configuration

--- aesthetic.el
+++ aesthetic.el
@@ -1,18 +1,42 @@
+;; aesthetic-emacs, 24.03.06.23.07
+;; Runs the aesthetic computer backend in emacs, for a nice dashboard.
+
+;; 📚 Notes
+;; for installing on fedora 39, do: `sudo dnf copr enable stevenlin/emacs-pgtk-nativecomp`
+
+;; ✍️ Basic Emacs Configuration
+(setq explicit-shell-file-name "/usr/share/fish")
+
+(setq inhibit-startup-screen t) ;; Disable startup message.
+(setq eshell-banner-message "") ;; No eshell banner.
+
+(menu-bar-mode -1) ;; Disable the menu bar.
+(tool-bar-mode -1) ;; Disable the tool bar.
+(load-theme 'wombat t) ;; Set a dark theme.
+
+(setq-default truncate-lines t) ;; Disable line wrapping by default.
+
+(fringe-mode 0) ;; Disable fringe indicators.
+(scroll-bar-mode -1) ;; Disable scroll bar.
+
+;; 🤖 Aesthetic Servers
+(defun start-eshell-with-command (buffer-name command)
+  "Start eshell in a buffer named BUFFER-NAME and run the specified COMMAND."
+  (let ((eshell-buffer (generate-new-buffer-name buffer-name)))
+    (eshell t)  ;; Start eshell in a new buffer
+    (rename-buffer eshell-buffer)  ;; Rename the eshell buffer
+    (insert command)  ;; Insert the command
+    (eshell-send-input)))  ;; Send the command to eshell
+
 (defun start-multiplex-environment ()
-  "Start a multiplexed environment with predefined commands."
+  "Start a multiplexed environment with predefined commands in eshell."
+  ;; Start the first eshell and run the first command
+  (start-eshell-with-command "server" "npm run aesthetic-emacs-top")
 
-  ;; Disable startup message
-  (setq inhibit-startup-screen t)
-
-  ;; Start the first terminal and run the first command
-  (ansi-term "/bin/bash")
-  (term-send-raw-string "npm run aesthetic -- --exclude server:session\n")
-
-  ;; Create a new window and start the second terminal
-  (split-window-below)
-  (other-window 1)
-  (ansi-term "/bin/bash")
-  (term-send-raw-string "npm run server:session\n"))
+  ;; Create a new window and start the second eshell
+  (split-window-below)  ;; Split the window
+  (other-window 1)  ;; Move focus to the new window
+  (start-eshell-with-command "session" "npm run aesthetic-emacs-bottom"))
 
 ;; Execute the function on startup
 (add-hook 'emacs-startup-hook 'start-multiplex-environment)
