9ef0f7f9968f02ef0f1681095133e62e46657a02
Jeffrey Alan Scudder <me@jas.life>
2024-03-23 20:13:42 -0700

    more docker

--- micro/code.sh
+++ micro/code.sh
@@ -1,9 +1,12 @@
 
 #!/usr/bin/env bash
 
-set -e # quit after any error
+# set -e # quit after any error
 
+echo "*** -> Resetting Docker State..."
 ./reset.sh # destory any existing container
+echo "*** -> Booting and Building AC..."
 ./boot.sh # rebuild the container from cache (or nothing)
           # and start it as a daemon
+echo "*** -> Starting a development session..."
 ./emacs.sh # start an emacs session from within the container
