454713d257bb9f3b8591f0d7f66428525529ecec
Jeffrey Alan Scudder <me@jas.life>
2024-03-24 01:28:54 +0000

    fix discord hook

--- micro/emacs.sh
+++ micro/emacs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -it aesthetic-container //usr/bin/fish -c "emacsclient -e '(kill-emacs)'; emacs --daemon; emacsclient -c --eval '(aesthetic-backend)'"
+docker exec -it aesthetic-container //usr/bin/fish -c "fish /entry.fish; emacsclient -e '(kill-emacs)'; emacs --daemon; emacsclient -c"
