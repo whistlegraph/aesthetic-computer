cefeb7aae484dc7bc6498f4dac0193b7592b761d
Jeffrey Alan Scudder <me@jas.life>
2024-03-23 20:13:42 -0700

    more docker

--- micro/emacs.sh
+++ micro/emacs.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-docker exec -it aesthetic-container //usr/bin/fish -c "fish /entry.fish; emacsclient -e '(kill-emacs)'; emacs --daemon; emacsclient -c"
+echo "Emacs client here we go..."
+
+docker exec -it aesthetic-container //usr/bin/fish -c "fish"
+
+# docker exec -it aesthetic-container //usr/bin/fish -c "emacsclient -e '(kill-emacs)'; emacs --daemon; emacsclient -c"
+# docker exec -it aesthetic-container //usr/bin/fish -c "fish /entry.fish"
