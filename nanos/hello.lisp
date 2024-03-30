de845baee6d94fc41bd9b36d9d64283d4c7d7df3
Jeffrey Alan Scudder <me@jas.life>
2024-03-30 13:40:03 -0700

    add socket support to nanos

--- nanos/hello.lisp
+++ nanos/hello.lisp
@@ -1,53 +1,25 @@
-(declaim (optimize (debug 3)))  ; Set the debugging level
-(sb-ext:enable-debugger)        ; Enable the SBCL debugger
+; This file is old now and was
+; meant for getting sbcl running in a unikernel.
 
-(format t "Hello, SBCL!~%")
+;(declaim (optimize (debug 3)))  ; Set the debugging level
+;(sb-ext:enable-debugger)        ; Enable the SBCL debugger
 
-;;(handler-case
-;;    (progn
-      (load "quicklisp.lisp")
-      (format t "Loaded quicklisp...~%")
-      
-      ;(quicklisp-quickstart:install)
-      (quicklisp-quickstart:install :path ".quicklisp/")
+(format t "Hello!~%")
 
-      (format t "Installed quicklisp...~%")
+(load "quicklisp.lisp")
 
-      ;; Uncomment if needed
-      ;; (load "quicklisp/setup.lisp")
+(cond
+ ((probe-file "./quicklisp/setup.lisp") ; Condition
+  (format t "Setup found...~%")
+  (load "./quicklisp/setup.lisp")       ; True branch
+  (format t "Quicklisp setup loaded.~%"))
+ (t                                    ; Else branch
+  (format t "Quicklisp setup not found.~%")
+  (quicklisp-quickstart:install :path "./quicklisp")
+  (format t "Installed quicklisp...~%")))
 
-      ; (ql:quickload "hunchentoot")
-;;      (format t "Loaded hunchentoot!~%"))
-;;  (error (e)
-;;    (format t "An error occurred: ~A~%" e)))  ; Print any caught error
+(ql:quickload "hunchentoot")
 
- 
-;(load "quicklisp.lisp")
-;(format t "Loaded quicklisp...")
+(format t "Loaded hunchentoot!")
 
-;(quicklisp-quickstart:install)
-
-;; (load "quicklisp/setup.lisp")
-
-
-;(format t "Installed quicklisp...")
-
-;(ql:quickload "hunchentoot")
-
-;(format t "Loaded hunchentoot!")
-
-;(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 8083))
-
-;(defun hello-world-handler (request)
-;  (declare (ignore request))
-;  (setf (hunchentoot:content-type*) "text/plain")
-;  (setf (hunchentoot:return-code*) 200)
-;  (format nil "Hello, World!"))
-;
-;(defun start-server ()
-;  (let ((dispatcher (hunchentoot:create-prefix-dispatcher "/" 'hello-world-handler)))
-;    (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor 
-;                                      :port 8083
-;                                      :dispatcher dispatcher))))
-
-;(start-server)
+(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 8083))
