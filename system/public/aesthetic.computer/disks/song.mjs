5bcaa99681432703f5c05a358f57c52fe5700ccb
Jeffrey Alan Scudder <me@jas.life>
2024-04-27 00:42:00 +0000

    transpose melody

--- system/public/aesthetic.computer/disks/song.mjs
+++ system/public/aesthetic.computer/disks/song.mjs
@@ -36,8 +36,8 @@ let btn,
 // G -1 C6 C6 D6 D6 E6 G6 E6 C6
 // G C6 C D D E G E C
 
-const mmm = `B F# B F# B F# B D# F#
-             E C# E C# E C# A# C# F#`.split(/\s+/);
+const mmm = `B F# B F# B F# B D#5 F#5
+             E5 C#5 E5 C#5 E5 C#5 A# C#5 F#`.split(/\s+/);
 
 //const pgtw = "F F G G A C6 A F C F F G C6 A".split(" ");
 const pgtw = `G C6 C6 D6 D6 E6 G6 E6 C6
