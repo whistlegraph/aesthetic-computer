f3caa6fb44e9ffe79802b762c56aa5fca38150f2
Jeffrey Alan Scudder <me@jas.life>
2024-07-16 19:11:01 +0000

    ucla-5 bgcolor

--- system/public/aesthetic.computer/disks/ucla-5.mjs
+++ system/public/aesthetic.computer/disks/ucla-5.mjs
@@ -1,5 +1,5 @@
-// ucla-5, 24.07.16.18.50 
-// Worms and clocks. 
+// ucla-5, 24.07.16.18.50
+// Worms and clocks.
 
 /* 📝 Notes 
   - Today we will be designing a worm-like form, in addition to a clock. 
@@ -7,13 +7,14 @@
     1. [☁️] Let's remember to finish... `source ucla-4-box`.
     2. [] Leading and following! 🟢🟥
     3. [] Expanding worm. 🪱
+    ---
     4. [] Keeping time and clock-making. 
     5. - [] Progress bars.
     6. - [] Learning `lineAngle(x1, y1, dist, degrees)`
 */
 
 function paint({ wipe }) {
-  wipe("gray");
+  wipe("orange");
 }
 
 // 📚 Library
