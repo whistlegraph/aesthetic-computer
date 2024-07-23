34e1622b2fab6400dc1843b41a072da2b91f5bff
Jeffrey Alan Scudder <me@jas.life>
2024-07-23 20:11:59 +0000

    rm shortcut and source code logs from headers

--- system/public/aesthetic.computer/lib/headers.mjs
+++ system/public/aesthetic.computer/lib/headers.mjs
@@ -1,7 +1,7 @@
 export function headers() {
   // Title
   console.log(
-    "%caesthetic.computer",
+    "%cAesthetic Computer",
     `background: rgba(10, 20, 40);
      color: rgb(200, 200, 250);
      font-size: 120%;
@@ -12,22 +12,22 @@ export function headers() {
   ); // Print a pretty title in the console.
 
   // Global Keyboard Shortcuts
-  console.log(
-    `%cFullscreen: C-x, Prompt: ~`,
-    `background-color: black;
-     color: rgb(200, 200, 200);
-     padding: 0 0.25em;
-     border-left: 0.75px solid rgb(60, 60, 60);
-     border-right: 0.75px solid rgb(60, 60, 60);`
-  );
+  // console.log(
+  //   `%cFullscreen: C-x, Prompt: ~`,
+  //   `background-color: black;
+  //    color: rgb(200, 200, 200);
+  //    padding: 0 0.25em;
+  //    border-left: 0.75px solid rgb(60, 60, 60);
+  //    border-right: 0.75px solid rgb(60, 60, 60);`
+  // );
 
   // Source code URL.
-  console.log(
-    "%chttps://github.com/whistlegraph/aesthetic-computer",
-    `color: rgb(200, 200, 200);
-     background-color: black;
-     padding: 0 0.25em;
-     border-left: 0.75px solid rgb(60, 60, 60);
-     border-right: 0.75px solid rgb(60, 60, 60);`
-  );
+  // console.log(
+  //   "%chttps://github.com/whistlegraph/aesthetic-computer",
+  //   `color: rgb(200, 200, 200);
+  //    background-color: black;
+  //    padding: 0 0.25em;
+  //    border-left: 0.75px solid rgb(60, 60, 60);
+  //    border-right: 0.75px solid rgb(60, 60, 60);`
+  // );
 }
