64041328e02f01e600467e5dbfe39dcfd47d5a3f
Jeffrey Alan Scudder <me@jas.life>
2024-07-30 20:21:51 +0000

    add ucla-7

---
+++ system/public/aesthetic.computer/disks/ucla-7.mjs
@@ -0,0 +1,49 @@
+// ucla-7, 24.07.30.20.20 
+// Forces 
+
+/* 📝 Notes 
+  - Today we will be working with forces.
+    15.  🎈 *BONUS* 🎶 Musical (Temporal) Systems
+    📃 Up Next...
+      Bouncing balls!
+    16. - [] Using parameters in Aesthetic Computer (make sure development works)
+*/
+
+function boot({ wipe, fps }) {
+  wipe("purple");
+}
+
+function paint({ ink }) {
+}
+
+// 📚 Library
+
+// function boot() {
+// Runs once at the start.
+// }
+
+// function act({ event: e }) {
+//  // Respond to user input here.
+// }
+
+// function sim() {
+//  // Runs once per logic frame. (120fps locked.)
+// }
+
+// function beat() {
+//   // Runs once per metronomic BPM.
+// }
+
+// function leave() {
+//  // Runs once before the piece is unloaded.
+// }
+
+// function preview({ ink, wipe }) {
+// Render a custom thumbnail image.
+// }
+
+// function icon() {
+// Render an application icon, aka favicon.
+// }
+
+// ⚠️ Also available: `brush` and `filter`.
