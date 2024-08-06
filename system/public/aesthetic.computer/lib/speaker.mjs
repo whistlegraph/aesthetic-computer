79fad3a6dc83a5af0413fc9845752e66da10b02d
jeffrey <me@jas.life>
2024-08-06 03:43:23 -0700

    Update speaker.mjs

--- system/public/aesthetic.computer/lib/speaker.mjs
+++ system/public/aesthetic.computer/lib/speaker.mjs
@@ -227,7 +227,7 @@ class SoundProcessor extends AudioWorkletProcessor {
       if (voices > 1) {
         if (!within(0.001, this.#mixDivisor, voices)) {
           if (this.#mixDivisor < voices) {
-            this.#mixDivisor += 0.001;
+            this.#mixDivisor += 0.0001;
           } else {
             this.#mixDivisor -= 0.01; //0.001; // 0.0001;
           }
