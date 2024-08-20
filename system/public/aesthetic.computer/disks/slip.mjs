829760df9f43aa6d2f3fa032ed191213e348cf1a
Jeffrey Alan Scudder <me@jas.life>
2024-08-20 23:52:41 +0000

    `slip` update

--- system/public/aesthetic.computer/disks/slip.mjs
+++ system/public/aesthetic.computer/disks/slip.mjs
@@ -2,14 +2,14 @@
 // A single voice instrument.
 
 /* 📝 Notes 
-  - [] Get the basic tone interaction right.
-  - [] Make volume go down when further from the center.
+  - [] add on-screen support for switching wave types?
   - [] Show a good minimap of current note etc.
   - [] Add pitch draggability across two octaves with proper notching.
   - [] Add a visualizer graph.
   - [] Add a two column / comparison mode?
-  - [] add on-screen support for switching wave types?
   + Done
+  - [x] Get the basic tone interaction right.
+  - [x] Make volume go down when further from the center.
   - [x] Create the tone while tapping.
 */
 
@@ -24,74 +24,109 @@ const top = 24;
 const margin = 14;
 const stretch = notes.length;
 let octave = "4";
-let voice, currentNote, height, section;
+let voice,
+  wave = "sine",
+  currentNote,
+  currentVolume = 0,
+  height,
+  section;
 
 function boot({ colon }) {
-  octave = colon[0] || "4";
+  const wavetypes = ["square", "sine", "triangle", "sawtooth", "noise-white"];
+  wave = wavetypes.indexOf(colon[0]) > -1 ? colon[0] : wave;
+  const newOctave = parseInt(colon[0]) || parseInt(colon[1]);
+  if (newOctave) octave = newOctave.toString();
 }
 
 function paint({ wipe, ink, line, screen, pen, num, help: { choose } }) {
   wipe(voice ? 150 : "gray");
   height = screen.height - top - 8;
-  section = height / stretch;
+  section = Math.round(height / stretch);
+  const hs = section / 2;
 
   notes.forEach((note, i) => {
-    const y = floor(top + i * section + section / 2);
+    const y = floor(top + i * section + hs);
     const sub = pen ? min(255, floor(pow(abs(pen.y - y), 1.2))) : 0;
 
     const caps = note.toUpperCase();
+    const x = 6; //screen.width / 2 - (caps.length * 6) / 2;
 
     if (note !== currentNote) {
+      if (!note.endsWith("#")) {
+        ink(200, 180, 100, 100).box(0, y - hs + 2, screen.width, section - 2);
+      }
+
       const cutoff = 160;
       if (sub < cutoff) {
-        ink("white", 255 - sub).write(caps, 6, y - 5);
+        ink("white", 255 - sub).write(caps, x, y - 5);
       } else {
-        ink("pink", 255 - cutoff).write(caps, 6, y - 5);
+        ink("pink", 255 - cutoff).write(caps, x, y - 5);
       }
 
       const sub2 = pen ? min(255, floor(pow(abs(pen.y - y), 1.4))) : 0;
-      if (sub2 < 255) ink("yellow", 255 - sub2).write(caps, 6, y - 5);
+      if (sub2 < 255) ink("yellow", 255 - sub2).write(caps, x, y - 5);
+      // if (voice && sub2 < 255) ink("yellow", 255 - sub2).write(caps, 6, y - 5);
+      // if (voice) ink("white").write(currentNote, 6, 18);
     } else {
+      ink(200, 200, 100).box(0, y - hs + 2, screen.width, section - 2);
+
       ink("lime", 128).write(
         caps,
-        6 + choose(-1, 0, 1),
+        x + choose(-1, 0, 1),
         y - 5 + choose(-1, 0, 1),
       );
       ink("red", 128).write(
         caps,
-        6 + choose(-1, 0, 1),
+        x + choose(-1, 0, 1),
         y - 5 + choose(-1, 0, 1),
       );
-      ink("white").write(caps, 6 + choose(-1, 0, 1), y - 5 + choose(-1, 0, 1));
+      ink("white").write(caps, x + choose(-1, 0, 1), y - 5 + choose(-1, 0, 1));
     }
 
-    // ink("white", 128).line(12 + margin, y, screen.width - 12, y);
-
-    ink("lime", 64).line(
-      0,
-      y - section / 2 + 1,
-      screen.width - 12,
-      y - section / 2 + 1,
-    );
-    ink("red", 64).line(0, y - section / 2, screen.width - 12, y - section / 2);
+    ink("lime", 64).line(0, y - hs + 1, screen.width, y - hs + 1);
+    ink("red", 64).line(0, y - hs, screen.width, y - hs);
   });
 
-  if (pen) ink("white").line(12 + margin, pen.y, screen.width - 12, pen.y);
+  ink("cyan", 64 + currentVolume * 128).line(
+    screen.width / 2,
+    0,
+    screen.width / 2,
+    screen.height,
+  );
+
+  if (pen)
+    ink(voice ? "white" : "yellow").line(
+      12 + margin,
+      pen.y,
+      screen.width - margin,
+      pen.y,
+    );
 }
 
-function act({ event: e, sound }) {
+function act({ event: e, sound, screen }) {
   // Respond to user input here.
   if (e.is("touch:1")) {
     let tone = mapNote(e);
     currentNote = tone;
-
     if (tone.startsWith("+")) {
       tone = tone.replace("+", parseInt(octave) + 1);
     } else {
       tone = octave + tone;
     }
 
-    voice = sound.synth({ type: "sine", tone, attack: 0.005, duration: "🔁" });
+    // TODO: Figure out volume based on x.
+    // TODO: Should the layout be in a central column?
+
+    const hw = screen.width / 2;
+    const volume = (currentVolume = 1 - abs(hw - e.x) / hw);
+
+    voice = sound.synth({
+      type: wave,
+      tone,
+      volume,
+      attack: 0.005,
+      duration: "🔁",
+    });
   }
 
   if (e.is("draw:1")) {
@@ -103,12 +138,14 @@ function act({ event: e, sound }) {
       } else {
         tone = octave + tone;
       }
-      voice.update({ tone, duration: 0.1 });
+      const hw = screen.width / 2;
+      const volume = (currentVolume = 1 - abs(hw - e.x) / hw);
+      voice.update({ tone, volume, duration: 0.05 });
     }
   }
 
   if (e.is("lift:1")) {
-    voice?.kill();
+    voice?.kill(0.35);
     currentNote = null;
     voice = null;
   }
