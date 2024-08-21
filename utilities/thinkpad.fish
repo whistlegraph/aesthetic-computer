6b15e862ae2be10b80383a071832946fe06929ff
Jeffrey Alan Scudder <me@jas.life>
2024-08-21 22:34:09 +0000

    echos

--- utilities/thinkpad.fish
+++ utilities/thinkpad.fish
@@ -3,6 +3,8 @@
 # ⚠️ This script is for installing Aesthetic Computer development dependencies on
 #    a Thinkpad running Fedora.
 
+echo "🟣 Installing VS Code package repositories"
+
 # Import the Microsoft GPG key for Visual Studio Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
@@ -12,24 +14,33 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 # Update the package cache
 dnf check-update
 
+echo "🟣 Installing `foot`, `google-chrome`, `code`, `docker`, `bpytop`, `emacs`, `rust`, `cargo` and `xxd`"
+
 # Install dependencies
 sudo dnf install -y foot google-chrome code docker bpytop emacs rust cargo xxd
 # todo: mkcert as well?
+
+echo "🟣 Enabling `docker` system services"
 
 # Enable and start Docker service
 sudo systemctl enable docker
 sudo systemctl start docker
 
+echo "🟣 Installing `brew`"
 # Install Linuxbrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo "🟣 Symlinking dotfiles"
 # Add Linuxbrew to PATH
 # set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
 # Symlink dotfiles
 bash ~/aesthetic-computer/dotfiles/symlink.sh
 
+echo "🟣 Sourcing fish config"
 # reload fish configuration, which will add brew and cargo to path, etc.
 source ~/.config/fish/config.fish
+
+echo "🟣 Installing `gnome-randr`, `starship`, `nvim`, and `fnm`"
 
 # Install gnome-randr using Cargo
 cargo install gnome-randr
@@ -47,11 +58,15 @@ brew install fnm
 fnm install --lts
 fnm default --lts
 
+echo "🟣 Installing `devcontainer` cli"
+
 # Install devcontainer CLI globally using npm
 npm install -g @devcontainers/cli
 
 # todo: Install mkcert certificates locally
 # mkcert -install
+
+echo "🟣 Installing `Fira Code` font"
 
 # Install system font (optional, depending on your needs)
 mkdir -p ~/.local/share/fonts
@@ -63,14 +78,15 @@ unzip FiraCode.zip -d FiraCode
 rm FiraCode.zip
 cd -
 
+echo "🟣 Installing `theme.sh` utility"
+
 # Install theme.sh tools
 curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M'
 chmod +x /usr/bin/theme.sh`
 
+echo "🟣 Installing Aesthetic Computer system daemon"
 # Install AC JavaScript system daemon for watching SSL and perhaps
 # performing other tasks... on command, which could accept
 # messages from the client.
 # Is this still being used actively? 24.08.21.21.46
 fish ~/aesthetic-computer/daemon/install_daemon.fish
-
-echo "All development dependencies installed successfully."
--- utilities/thinkpad.sh
+++ utilities/thinkpad.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "🟠 Beginning `Aesthetic Computer` System Installation"
+
+echo "🟣 Installing fish shell"
+
 # Install Fish shell
 sudo dnf install -y fish
 
@@ -9,6 +13,4 @@ chsh -s /usr/bin/fish
 # Run the Fish script to install other software
 fish /home/me/aesthetic-computer/utilities/thinkpad.fish
 
-echo "Fish shell installed, set as default, and additional software installed."
-
-echo "Please log out and log back in for the shell change to take effect."
+echo "🟢 Installation complete"
