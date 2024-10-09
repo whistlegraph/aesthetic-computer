3dd3851360c6584eded8b4b922e47450699a24a5
Jeffrey Alan Scudder <me@jas.life>
2024-10-09 18:13:29 +0000

    add support for subscription receipt emails to `sotce-net`

--- system/backend/email.mjs
+++ system/backend/email.mjs
@@ -10,18 +10,20 @@ export async function email(options) {
       pass: process.env.SMTP_PASS,
     },
   };
-  options.from = process.env.SMTP_USER;
+
   if (options.auth) {
     config.auth = options.auth;
     delete options.auth;
   }
+
+  options.from = config.auth.user;
 
   try {
     const transporter = nodemailer.createTransport(config);
     await transporter.sendMail(options);
     return true;
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.error("❌📧 Error sending email:", error);
     return false;
   }
 }
