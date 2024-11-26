ffae3b24715e0673e942138a276f5c35fbe9ca54
Jeffrey Alan Scudder <me@jas.life>
2024-11-26 08:33:27 +0000

    add white noise to `rattle`

--- system/public/aesthetic.computer/disks/rattle.mjs
+++ system/public/aesthetic.computer/disks/rattle.mjs
@@ -8,7 +8,7 @@
  - [🍊] Add accelerometer information.
 #endregion */
 
-const { abs } = Math;
+const { abs, min, max } = Math;
 
 const release = 0;
 let catch1, catch2, catch3;
@@ -43,8 +43,18 @@ function paint({ wipe, ink, motion, screen, sound: { synth } }) {
 // 🎪 Act
 function act({ event, motion }) {}
 
+let t1, t2, t3;
+const bass1 = 840;
+const bass2 = 850;
+const bass3 = 860;
+let t1t = 0,
+  t2t = 0,
+  t3t = 0;
+const lo = 10;
+const hi = 900;
+
 // 🧮 Sim
-function sim({ motion, pen, sound: { synth } }) {
+function sim({ num, motion, pen, sound: { synth } }) {
   const mo = motion.current;
   if (mo) {
     values = {
@@ -59,8 +69,8 @@ function sim({ motion, pen, sound: { synth } }) {
         z: mo?.accelWithGravity?.z.toFixed(2),
       },
       rotation: {
-        alpha: mo?.rotation?.alpha.toFixed(2) || (pen?.delta.x * 5),
-        beta: mo?.rotation?.beta.toFixed(2) || (pen?.delta.y * 5),
+        alpha: mo?.rotation?.alpha.toFixed(2) || pen?.delta.x * 5,
+        beta: mo?.rotation?.beta.toFixed(2) || pen?.delta.y * 5,
         gamma: mo?.rotation?.gamma.toFixed(2),
       },
     };
@@ -91,26 +101,104 @@ function sim({ motion, pen, sound: { synth } }) {
     if (catch2 < release) catch2 += 1;
     if (catch3 < release) catch3 += 1;
 
+    if (!t1) {
+      t1 = synth({
+        type: "noise-white",
+        tone: t1t, // 50 * abs(values.rotation.alpha),
+        volume: 0,
+        duration: "🔁",
+      });
+    }
+
+    if (!t2) {
+      t2 = synth({
+        type: "noise-white",
+        tone: t2t, // 100 * abs(values.rotation.beta),
+        volume: 0,
+        duration: "🔁",
+      });
+    }
+
+    if (!t3) {
+      t3 = synth({
+        type: "noise-white",
+        tone: t3t, // 25 * abs(values.rotation.gamma),
+        volume: 0,
+        duration: "🔁",
+      });
+    }
+
+    const div = 10;
+
+    function calvol(val) {
+      return max(0.0, min(1, abs(val * val) * 50));
+    }
+
+    {
+      const val = parseFloat(values.rotation.alpha);
+      t1t += val / div;
+      t1t = num.lerp(t1t, 0, 0.0353);
+      values.t1t = bass1 + abs(t1t);
+      t1?.update({
+        tone: values.t1t,
+        volume: calvol(t1t / (bass1 * 2)),
+        duration: 0.005,
+      });
+    }
+
+    {
+      const val = parseFloat(values.rotation.beta);
+      t2t += val / div;
+      t2t = num.lerp(t2t, 0, 0.0353);
+      values.t2t = bass2 + abs(t2t);
+      t2?.update({
+        tone: values.t2t,
+        volume: calvol(t2t / (bass2 * 2)),
+        duration: 0.005,
+      });
+    }
+
+    {
+      const val = parseFloat(values.rotation.gamma);
+      t3t += val / div;
+      t3t = num.lerp(t3t, 0, 0.0353);
+      values.t3t = bass3 + abs(t3t);
+      t3?.update({
+        tone: values.t3t,
+        volume: calvol(t3t / (bass3 * 2)),
+        duration: 0.005,
+      });
+    }
+
+    /*
     if (abs(values.rotation.alpha) > 20) {
       if (catch1 === release) {
-        makePerc(50 * values.rotation.alpha);
+        const tone = 50 * abs(values.rotation.alpha);
+        t1?.update({ tone: tone, duration: 0.001 });
         catch1 = 0;
       }
     }
+    */
 
+    /*
     if (abs(values.rotation.beta) > 20) {
       if (catch2 === release) {
-        makePerc(100 * values.rotation.beta);
+        const tone = 100 * abs(values.rotation.beta);
+        makePerc(tone);
         catch2 = 0;
       }
     }
+    */
 
+    /*
     if (abs(values.rotation.gamma) > 20) {
       if (catch3 === release) {
-        makePerc(25 * values.rotation.gamma);
+        const tone = 25 * abs(values.rotation.gamma);
+        makePerc(tone);
         catch3 = 0;
       }
     }
+    */
   }
 }
 
