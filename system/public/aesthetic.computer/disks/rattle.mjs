58544ed810a71f7218605f9d06d571ba455e606d
jeffrey <me@jas.life>
2024-11-26 15:36:48 -0800

    Update rattle.mjs

--- system/public/aesthetic.computer/disks/rattle.mjs
+++ system/public/aesthetic.computer/disks/rattle.mjs
@@ -256,11 +256,11 @@ function sim({ num, motion, pen, sound: { synth } }) {
       const val = (abs(parseFloat(values.accel.x)) + abs(parseFloat(values.accel.y)) + abs(parseFloat(values.accel.z)))/3 ;
       t4t += val;
       t4t = num.lerp(t4t, 0, 0.065);
-      values.t4t = t4t;
+      values.t4t = -25 + t4t;
       const vol = calvol(t4t / 50);
       values.vol = t4t / 50;
       t4?.update({
-        tone: values.t4t,
+        tone: 800 + values.t4t * 4,
         volume: values.vol,
         duration: 0.005,
       });
