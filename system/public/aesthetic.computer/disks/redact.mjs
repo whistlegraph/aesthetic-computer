0a2e891dbe026719e2685eb6bc5b10c9b66cdb14
Jeffrey Alan Scudder <me@jas.life>
2025-02-09 03:56:40 +0000

    fix redact w/ cp

---
+++ system/public/aesthetic.computer/disks/redact.mjs
@@ -0,0 +1,19 @@
+// redact, 25.02.08.01.45
+// Used for redacting user information like chat messages and masking it
+// before rendering and/or sending to clients.
+
+// Modify a message object into a 'redacted' state.
+export function redact(msg) {
+  // console.log("💌 Redacting:", msg);
+  msg.redactedText = msg.text;
+  msg.text = msg.text.replace(/\S/g, "_"); // Replace non-space chars with
+  //                                          underscores.
+  // msg.from = "redacted";
+}
+
+// Revert a message out of a 'redacted' state.
+export function unredact(msg) {
+  // console.log("💌 Unredacting:", msg);
+  msg.text = msg.redactedText || msg.text;
+  // msg.from = "redacted";
+}
