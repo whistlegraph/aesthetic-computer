8c1bcbd07ffb6c288ceeab9a14f31834671f4d2a
Jeffrey Alan Scudder <me@jas.life>
2025-05-01 22:52:58 +0000

    small notebook updates and devcontainer module resolution

--- notebook/aesthetic.py
+++ notebook/aesthetic.py
@@ -10,5 +10,6 @@ from IPython.display import display, IFrame
 def show(piece, width="100%", height=54):
     importlib.reload(importlib.import_module('aesthetic'))
     # Ensure parameters are separated explicitly in the URL
-    url = f"https://localhost:8888/{piece}?nolabel&nogap"
+    url = f"https://aesthetic.computer/{piece}?nolabel&nogap"
+    # url = f"https://localhost:8888/{piece}?nolabel&nogap"
     display(IFrame(src=url, width=width, height=height, frameborder="0"))
