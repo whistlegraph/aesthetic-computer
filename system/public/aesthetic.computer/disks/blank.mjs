7a57d09f847eab584f0670b734654b4e5380345b
Jeffrey Alan Scudder <me@jas.life>
2025-06-04 04:24:49 +0000

    add frac prototype and paintball

--- system/public/aesthetic.computer/disks/blank.mjs
+++ system/public/aesthetic.computer/disks/blank.mjs
@@ -1,9 +1,12 @@
 // $NAME, $TIMESTAMP
 // $THIS_IS_A_TEMPLATE_FOR_MAKING_NEW_PIECES
 
-/* 📝 Notes
- */
-
+/* 📝 Engineering Notes
+  The `paint` function runs every animation frame.
+  `screen.pixels` is a Uint8ClampedArray with direct access.
+  `screen.width` and `screen.height` is also available for aspect ratio / limits.
+  Special note: `Use screen.pixels / direct pixel array access for any automated drawing.`
+*/
 
 function paint({ api, wipe, ink, line, screen, box, circle, pen, write }) {
   wipe("gray"); // Clear the background.
