5e4ebafd25b90b18dc20bb58d7531cb2e2e48530
Jeffrey Alan Scudder <me@jas.life>
2025-06-20 10:35:50 +0000

    cleanup

--- system/public/aesthetic.computer/disks/kidlisp-wip.lisp
+++ system/public/aesthetic.computer/disks/kidlisp-wip.lisp
@@ -1,7 +1,11 @@
 ; kidlisp-wip, 25.06.20.02.38
 ; 🚸 Working Developments for the Kid Lisp API
 
+(def slide 1)
+
 ; 🏡 Stripes, 25.06.20.08.33
+(if (= slide 1)
+  (wipe blue)
 ;; label "'stripes', 25.06.20.08.33"
 ;; (ink rainbow (? 45 128 20))
 ;; (0.028s 
@@ -9,24 +13,25 @@
 ;;   (box 0 0 width/16 height)
 ;; )
 ;; (shear 0.00343 (? 0.03 0.1 -1))
+)
 
 ; 🚋 Train, 25.06.20.07.29
-label "'train', 25.06.20.07.29"
-(0.5s (zoom 0.5))
-mask 0 frame%height (? 1 10) height/4 
-blur 7
-unmask
-(0.09s
-  (ink
-   (? white black rainbow white black)
-   (? 6 12 50 50 150 6 6 6 6 6 50 50 50 50 70 230))
-  (box 0 0 width/2 height/2)
-)
-ink rainbow 16
-(repeat 3 (line))
-(0.5s (zoom (? 0.9 1.2)))
-scroll width/5.67666*0.828 
-(8s (mask 0 height/2 width height/2) (sort) (unmask))
+;; label "'train', 25.06.20.07.29"
+;; (0.5s (zoom 0.5))
+;; mask 0 frame%height (? 1 10) height/4 
+;; blur 7
+;; unmask
+;; (0.09s
+;;   (ink
+;;    (? white black rainbow white black)
+;;    (? 6 12 50 50 150 6 6 6 6 6 50 50 50 50 70 230))
+;;   (box 0 0 width/2 height/2)
+;; )
+;; ink rainbow 16
+;; (repeat 3 (line))
+;; (0.5s (zoom (? 0.9 1.2)))
+;; scroll width/5.67666*0.828 
+;; (8s (mask 0 height/2 width height/2) (sort) (unmask))
 
 ; 🌪️ Spin cycle, 25.06.20.08.34
 ;; label "'spin cycle', 25.06.20.08.34"
