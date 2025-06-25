a96335847698309a0db2483ee179b47cd359e9a1
Jeffrey Alan Scudder <me@jas.life>
2025-06-25 10:27:14 +0000

    kidlisp tests

--- system/public/aesthetic.computer/disks/kidlisp-wip.lisp
+++ system/public/aesthetic.computer/disks/kidlisp-wip.lisp
@@ -1,15 +1,14 @@
 ; kidlisp-wip, 25.06.20.02.38
 ; 🚸 Working Developments for the Kid Lisp API
 
-(def slide 0)
-
+(def slide 0) ; 🛝 Slideshow Mechanism
 (5s (now slide (% slide+1 4)))
 
-; 🧪 Testing improved repeat function with iterator variables
-(if (= slide 0) (label) (wipe black) (ink white) (write "3 kidlisp tests" center center))
+; 🪧 Title Screen
+(if (= slide 0) (label) (wipe black) (ink white)
+ (write "3 kidlisp tests" center center))
 
 ; 🏡 Stripes, 25.06.20.08.33
-;wipe
 (if (= slide 1)
   (label "'stripes', 25.06.20.08.33")
   (ink rainbow (? 45 128 20))
@@ -54,61 +53,3 @@
   (spin 0.28888)
   (scroll 0.5)
 )
-
-(if (= slide 4)
- (noise) 
-)
-
-; ====
-
-; 🎤 Microphone Tests, 25.06.22
-(if (= slide 5)
-  ; Simple microphone amplitude test
-  (wipe "black")
-  (def amp (mic))
-  (ink "white")
-  (write "Mic Level:" 6 24)
-  (ink "cyan")
-  (write amp 150 24)
-  (ink "lime")
-  (box 10 40 (* amp 200) 30)
-)
-
-(if (= slide 6)
-  ; Microphone visualizer
-  (wipe "black")
-  (def amp (mic))
-  (def spkr (amplitude))
-  
-  ; Display both values for comparison
-  (ink "white")
-  (write "Microphone Level:" 10 24)
-  (ink "cyan")
-  (write amp 200 24)
-  
-  (ink "white")
-  (write "Speaker Level:" 10 50)
-  (ink "orange")
-  (write spkr 200 50)
-  
-  ; Visual amplitude bar (use speaker if mic is 0)
-  (def activeAmp (if (> amp 0) amp spkr))
-  (ink "lime")
-  (box 10 80 (* activeAmp 200) 30)
-  
-  ; Amplitude meter with scale
-  (ink "gray")
-  (box 10 120 200 20 "out")
-  (ink "red")
-  (box 10 120 (* activeAmp 200) 20)
-  
-  ; Responsive background color based on amplitude
-  (wipe (+ 50 (* activeAmp 100)) 0 (+ 100 (* activeAmp 100)))
-  
-  ; Circle that grows with amplitude
-  (ink "yellow")
-  (def radius (+ 20 (* activeAmp 50)))
-  (box (- 150 radius) (- 150 radius) (* radius 2) (* radius 2))
-)
-
-; sonify
