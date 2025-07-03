1881fa412906947b48b7ec8944e039b906ee5e1d
Jeffrey Alan Scudder <me@jas.life>
2025-07-03 21:54:32 +0000

    dev environment prompts and modes in the repo / scaffold ac prompts

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -9,25 +9,9 @@ set -eu
 create_symlinks() {
   # Get the directory in which this script lives.
   script_dir=$(dirname "$(readlink -f "$0")")
-
-  # link fish config
-  rm -rf ~/.config/fish
-  ln -s $script_dir/dot_config/fish ~/.config/fish
-
-  # link vim config
-  rm -rf ~/.config/nvim
-  ln -s $script_dir/dot_config/nvim ~/.config/nvim
-
-  # link starship config
-  rm -rf ~/.config/starship.toml
-  ln -s $script_dir/dot_config/starship.toml ~/.config/starship.toml
-
-  # link foot (a linux terminal client) config
-  rm -rf ~/.config/foot
-  ln -s $script_dir/dot_config/foot ~/.config/foot
-
-  # emacs
-  ln -s $script_dir/dot_config/emacs.el ~.emacs
+  
+  # Call the dedicated symlink script to avoid duplication
+  "$script_dir/symlink.sh"
 }
 
 install_fish() {
