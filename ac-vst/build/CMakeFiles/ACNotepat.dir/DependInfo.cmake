9f6c708bca4e8fbd99b480d2214fbc4927bbc44d
Jeffrey Alan Scudder <me@jas.life>
2025-11-30 21:42:47 +0000

    Add timestamp 1342 to plugin name

--- ac-vst/build/CMakeFiles/ACNotepat.dir/DependInfo.cmake
+++ ac-vst/build/CMakeFiles/ACNotepat.dir/DependInfo.cmake
@@ -10,6 +10,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/Users/jas/Desktop/code/aesthetic-computer/ac-vst/source/acnotepat.cpp" "CMakeFiles/ACNotepat.dir/source/acnotepat.cpp.o" "gcc" "CMakeFiles/ACNotepat.dir/source/acnotepat.cpp.o.d"
   "/Users/jas/Desktop/code/aesthetic-computer/ac-vst/source/acnotepat_entry.cpp" "CMakeFiles/ACNotepat.dir/source/acnotepat_entry.cpp.o" "gcc" "CMakeFiles/ACNotepat.dir/source/acnotepat_entry.cpp.o.d"
+  "/Users/jas/Desktop/code/aesthetic-computer/ac-vst/vst3sdk/public.sdk/source/main/macmain.cpp" "CMakeFiles/ACNotepat.dir/vst3sdk/public.sdk/source/main/macmain.cpp.o" "gcc" "CMakeFiles/ACNotepat.dir/vst3sdk/public.sdk/source/main/macmain.cpp.o.d"
   "/Users/jas/Desktop/code/aesthetic-computer/ac-vst/source/acnotepat_controller.mm" "CMakeFiles/ACNotepat.dir/source/acnotepat_controller.mm.o" "gcc" "CMakeFiles/ACNotepat.dir/source/acnotepat_controller.mm.o.d"
   )
 
