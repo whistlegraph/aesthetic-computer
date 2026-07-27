a59edb6beba5ce811cf81733b764f409f1ed7d53
prompt.ac/@jeffrey <mail@aesthetic.computer>
2026-07-27 00:08:41 -0700

    Serve Danz Ballet as a standalone canvas site

--- system/public/aesthetic.computer/lib/disk.mjs
+++ system/public/aesthetic.computer/lib/disk.mjs
@@ -249,8 +249,6 @@ const SAME_ORIGIN_BUILT_IN_PIECE_HOSTS = new Set([
   "www.kidlisp.com",
   "notepat.com",
   "www.notepat.com",
-  "danzballet.studio",
-  "www.danzballet.studio",
   "laklok.com",
   "www.laklok.com",
   "p5.aesthetic.computer",
